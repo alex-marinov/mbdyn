3f8d5bc2139f537085bcd49404c70da773617676
masarati <>
2001-03-23 16:38:32 +0000

    sistemato il template

--- mbdyn/modules/module-template/module-template.cc
+++ mbdyn/modules/module-template/module-template.cc
@@ -1,234 +1,338 @@
+#include <mbconfig.h>
+
 #include <loadable.h>
 
+/*
+ * user-defined struct
+ */
 struct module_template {
-   // user-defined struct
-   int i; 
+	int i; 
 };
 
-/* funzioni di default */
-void* read(LoadableElem* pEl,
-	   DataManager* pDM,
-	   MBDynParser& HP,
-	   const DriveHandler* pDH)
+/* default funcs */
+static void *
+read(
+		LoadableElem* pEl,
+		DataManager* pDM,
+		MBDynParser& HP,
+		const DriveHandler* pDH
+)
 {
-   DEBUGCOUTFNAME("read");
-   
-   // allocation of user-defined struct
-   module_template* p = NULL;
-   SAFENEW(p, module_template, EMmm);
-
-   // read data
-   
-   return (void *)p;
+	DEBUGCOUTFNAME("read");
+	
+	/*
+	 * allocation of user-defined struct
+	 */
+	module_template* p = NULL;
+	SAFENEW(p, module_template, EMmm);
+	
+	/*
+	 * read data
+	 */
+	if (HP.IsKeyWord("help")) {
+		cout << "Module; template" << endl;
+	}
+	
+	return (void *)p;
 }
 
-unsigned int i_get_num_dof(const LoadableElem* pEl)
+static unsigned int
+i_get_num_dof(const LoadableElem* pEl)
 {
-   DEBUGCOUTFNAME("i_get_num_dof");
-   return 0;
+	DEBUGCOUTFNAME("i_get_num_dof");
+	return 0;
 }
 
-DofOrder::Order set_dof(const LoadableElem*, unsigned int i)
+static DofOrder::Order
+set_dof(const LoadableElem*, unsigned int i)
 {
-   DEBUGCOUTFNAME("set_dof");
-   return DofOrder::UNKNOWN;
+	DEBUGCOUTFNAME("set_dof");
+	return DofOrder::UNKNOWN;
 }
 
-void output(const LoadableElem* pEl, OutputHandler& OH)
+static void
+output(const LoadableElem* pEl, OutputHandler& OH)
 {
-   DEBUGCOUTFNAME("output");
+	DEBUGCOUTFNAME("output");
 }
 
-ostream& restart(const LoadableElem* pEl, ostream& out)
+static ostream&
+restart(const LoadableElem* pEl, ostream& out)
 {
-   DEBUGCOUTFNAME("restart");
-   return out << "not implemented yet;" << endl;
+	DEBUGCOUTFNAME("restart");
+	return out << "not implemented yet;" << endl;
 }
 
-void work_space_dim(const LoadableElem* pEl, 
-		    integer* piNumRows, 
-		    integer* piNumCols)
+static void
+work_space_dim(const LoadableElem* pEl, integer* piNumRows, integer* piNumCols)
 {
-   DEBUGCOUTFNAME("work_space_dim");
-   *piNumRows = 0;
-   *piNumCols = 0;
+	DEBUGCOUTFNAME("work_space_dim");
+	*piNumRows = 0;
+	*piNumCols = 0;
 }
 
-VariableSubMatrixHandler& 
-ass_jac(LoadableElem* pEl, 
-	VariableSubMatrixHandler& WorkMat,
-	doublereal dCoef, 
-	const VectorHandler& XCurr,
-	const VectorHandler& XPrimeCurr)
+static VariableSubMatrixHandler& 
+ass_jac(
+		LoadableElem* pEl, 
+		VariableSubMatrixHandler& WorkMat,
+		doublereal dCoef, 
+		const VectorHandler& XCurr,
+		const VectorHandler& XPrimeCurr
+)
 {  
-   DEBUGCOUTFNAME("ass_jac");
-   integer iNumRows = 0;
-   integer iNumCols = 0;
-   pEl->WorkSpaceDim(&iNumRows, &iNumCols);
-   
-   FullSubMatrixHandler& WM = WorkMat.SetFull();
-   WM.ResizeInit(iNumRows, iNumCols, 0.);
-   
-   module_template* p = (module_template *)pEl->pGetData();
-   
-   // set sub-matrix indices and coefs
-   
-   return WorkMat;
+	DEBUGCOUTFNAME("ass_jac");
+	integer iNumRows = 0;
+	integer iNumCols = 0;
+	pEl->WorkSpaceDim(&iNumRows, &iNumCols);
+	
+	FullSubMatrixHandler& WM = WorkMat.SetFull();
+	WM.ResizeInit(iNumRows, iNumCols, 0.);
+
+#if 0
+	module_template* p = (module_template *)pEl->pGetData();
+#endif /* 0 */
+	
+	/*
+	 * set sub-matrix indices and coefs
+	 */
+	
+	return WorkMat;
 }
 
-void
-ass_eig(LoadableElem* pEl, 
-	VariableSubMatrixHandler& WorkMatA,
-	VariableSubMatrixHandler& WorkMatB,
-	const VectorHandler& XCurr,
-	const VectorHandler& XPrimeCurr)
+static void
+ass_eig(
+		LoadableElem* pEl, 
+		VariableSubMatrixHandler& WorkMatA,
+		VariableSubMatrixHandler& WorkMatB,
+		const VectorHandler& XCurr,
+		const VectorHandler& XPrimeCurr
+)
 {  
-   DEBUGCOUTFNAME("ass_eig");
-   integer iNumRows = 0;
-   integer iNumCols = 0;
-   pEl->WorkSpaceDim(&iNumRows, &iNumCols);
-   
-   FullSubMatrixHandler& WMA = WorkMatA.SetFull();
-   WMA.ResizeInit(iNumRows, iNumCols, 0.);
-   
-   FullSubMatrixHandler& WMB = WorkMatB.SetFull();
-   WMB.ResizeInit(iNumRows, iNumCols, 0.);
-   
-   module_template* p = (module_template *)pEl->pGetData();
-   
-   // set sub-matrix indices and coefs
+	DEBUGCOUTFNAME("ass_eig");
+	integer iNumRows = 0;
+	integer iNumCols = 0;
+	pEl->WorkSpaceDim(&iNumRows, &iNumCols);
+	
+	FullSubMatrixHandler& WMA = WorkMatA.SetFull();
+	WMA.ResizeInit(iNumRows, iNumCols, 0.);
+	
+	FullSubMatrixHandler& WMB = WorkMatB.SetFull();
+	WMB.ResizeInit(iNumRows, iNumCols, 0.);
+
+#if 0
+	module_template* p = (module_template *)pEl->pGetData();
+#endif /* 0 */
+	
+	/*
+	 * set sub-matrix indices and coefs
+	 */
 }
 
-SubVectorHandler& 
-ass_res(LoadableElem* pEl, 
-	SubVectorHandler& WorkVec,
-	doublereal dCoef,
-	const VectorHandler& XCurr, 
-	const VectorHandler& XPrimeCurr)
+static SubVectorHandler& 
+ass_res(
+		LoadableElem* pEl, 
+		SubVectorHandler& WorkVec,
+		doublereal dCoef,
+		const VectorHandler& XCurr, 
+		const VectorHandler& XPrimeCurr
+)
 {
-   DEBUGCOUTFNAME("ass_res");
-   integer iNumRows = 0;
-   integer iNumCols = 0;
-   pEl->WorkSpaceDim(&iNumRows, &iNumCols);
-   
-   WorkVec.Resize(iNumRows);
-   
-   module_template* p = (module_template *)pEl->pGetData(); 
+	DEBUGCOUTFNAME("ass_res");
+	integer iNumRows = 0;
+	integer iNumCols = 0;
+	pEl->WorkSpaceDim(&iNumRows, &iNumCols);
+	
+	WorkVec.Resize(iNumRows);
 
-   // set sub-vector indices and coefs
-   
-   return WorkVec;
+#if 0
+	module_template* p = (module_template *)pEl->pGetData(); 
+#endif /* 0 */
+	
+	/*
+	 * set sub-vector indices and coefs
+	 */
+	
+	return WorkVec;
 }
 
-void before_predict(const LoadableElem* pEl, 
-		    VectorHandler& X,
-		    VectorHandler& XP,
-		    VectorHandler& XPrev,
-		    VectorHandler& XPPrev)
+static void
+before_predict(
+		const LoadableElem* pEl, 
+		VectorHandler& X,
+		VectorHandler& XP,
+		VectorHandler& XPrev,
+		VectorHandler& XPPrev
+)
 {
-   DEBUGCOUTFNAME("before_predict");
+	DEBUGCOUTFNAME("before_predict");
 }
 
-void after_predict(const LoadableElem* pEl, 
-		   VectorHandler& X,
-		   VectorHandler& XP)
+static void
+after_predict(
+		const LoadableElem* pEl, 
+		VectorHandler& X,
+		VectorHandler& XP
+)
 {
-   DEBUGCOUTFNAME("after_predict");
+	DEBUGCOUTFNAME("after_predict");
 }
 
-void update(LoadableElem* pEl, 
-	    const VectorHandler& X,
-	    const VectorHandler& XP)
+static void
+update(
+		LoadableElem* pEl, 
+		const VectorHandler& X,
+		const VectorHandler& XP
+)
 {
-   DEBUGCOUTFNAME("update");
+	DEBUGCOUTFNAME("update");
 }
 
-unsigned int i_get_initial_num_dof(const LoadableElem* pEl)
+static unsigned int
+i_get_initial_num_dof(const LoadableElem* pEl)
 {
-   DEBUGCOUTFNAME("i_get_initial_num_dof");
-   return 0;
+	DEBUGCOUTFNAME("i_get_initial_num_dof");
+	return 0;
 }
 
-void initial_work_space_dim(const LoadableElem* pEl, 
-			    integer* piNumRows, 
-			    integer* piNumCols)
+static void
+initial_work_space_dim(
+		const LoadableElem* pEl, 
+		integer* piNumRows, 
+		integer* piNumCols
+)
 {
-   DEBUGCOUTFNAME("initial_work_space_dim");
-   *piNumRows = 0;
-   *piNumCols = 0;   
+	DEBUGCOUTFNAME("initial_work_space_dim");
+	*piNumRows = 0;
+	*piNumCols = 0;   
 }
 
-VariableSubMatrixHandler& 
-initial_ass_jac(LoadableElem* pEl, 
+static VariableSubMatrixHandler& 
+initial_ass_jac(
+		LoadableElem* pEl, 
 		VariableSubMatrixHandler& WorkMat, 
-		const VectorHandler& XCurr)
+		const VectorHandler& XCurr
+)
 {
-   DEBUGCOUTFNAME("initial_ass_jac");
-   integer iNumRows = 0;
-   integer iNumCols = 0;
-   pEl->InitialWorkSpaceDim(&iNumRows, &iNumCols);
-   
-   FullSubMatrixHandler& WM = WorkMat.SetFull();
-   WM.ResizeInit(iNumRows, iNumCols, 0.);
-   
-   module_template* p = (module_template *)pEl->pGetData();
-   
-   // set sub-matrix indices and coefs
-   
-   return WorkMat;
+	DEBUGCOUTFNAME("initial_ass_jac");
+	integer iNumRows = 0;
+	integer iNumCols = 0;
+	pEl->InitialWorkSpaceDim(&iNumRows, &iNumCols);
+	
+	FullSubMatrixHandler& WM = WorkMat.SetFull();
+	WM.ResizeInit(iNumRows, iNumCols, 0.);
+	
+#if 0
+	module_template* p = (module_template *)pEl->pGetData();
+#endif /* 0 */
+	
+	/*
+	 * set sub-matrix indices and coefs
+	 */
+	
+	return WorkMat;
 }
 
-SubVectorHandler& 
-initial_ass_res(LoadableElem* pEl, 
+static SubVectorHandler& 
+initial_ass_res(
+		LoadableElem* pEl, 
 		SubVectorHandler& WorkVec, 
-		const VectorHandler& XCurr)
+		const VectorHandler& XCurr
+)
 {
-   DEBUGCOUTFNAME("initial_ass_res");
-   integer iNumRows = 0;
-   integer iNumCols = 0;
-   pEl->WorkSpaceDim(&iNumRows, &iNumCols);
-   
-   WorkVec.Resize(iNumRows);
-   
-   module_template* p = (module_template *)pEl->pGetData(); 
-
-   // set sub-vector indices and coefs
-   
-   return WorkVec;
+	DEBUGCOUTFNAME("initial_ass_res");
+	integer iNumRows = 0;
+	integer iNumCols = 0;
+	pEl->WorkSpaceDim(&iNumRows, &iNumCols);
+	
+	WorkVec.Resize(iNumRows);
+	
+#if 0
+	module_template* p = (module_template *)pEl->pGetData(); 
+#endif /* 0 */
+	
+	/*
+	 * set sub-vector indices and coefs
+	 */
+	
+	return WorkVec;
 }
 
-void set_value(const LoadableElem* pEl, VectorHandler& X, VectorHandler& XP)
+static void
+set_value(const LoadableElem* pEl, VectorHandler& X, VectorHandler& XP)
 {
-   DEBUGCOUTFNAME("set_value");
+	DEBUGCOUTFNAME("set_value");
 }
    
-void set_initial_value(const LoadableElem* pEl, VectorHandler& X)
+static void
+set_initial_value(const LoadableElem* pEl, VectorHandler& X)
 {
-   DEBUGCOUTFNAME("set_initial_value");
+	DEBUGCOUTFNAME("set_initial_value");
 }
 
-unsigned int i_get_num_priv_data(const LoadableElem* pEl)
+static unsigned int
+i_get_num_priv_data(const LoadableElem* pEl)
 {
-   DEBUGCOUTFNAME("i_get_num_priv_data");
-   return 0;
+	DEBUGCOUTFNAME("i_get_num_priv_data");
+	return 0;
 }
 
-doublereal d_get_priv_data(const LoadableElem* pEl, unsigned int i)
+static doublereal
+d_get_priv_data(const LoadableElem* pEl, unsigned int i)
 {
-   DEBUGCOUTFNAME("d_get_priv_data");
-   ASSERT(pEl->iGetNumPrivData() > 0);
-   if (i > pEl->iGetNumPrivData()) {
-      cerr << "Module-template Elem: illegal private data index " << i << endl;      
-      THROW(ErrGeneric());
-   }
+	DEBUGCOUTFNAME("d_get_priv_data");
+	ASSERT(pEl->iGetNumPrivData() > 0);
+	if (i > pEl->iGetNumPrivData()) {
+		cerr << "Module-template Elem: illegal private data index "
+			<< i << endl;      
+		THROW(ErrGeneric());
+	}
    
-   // return i-th priv data
-   return 0.;
+	/*
+	 * return i-th priv data
+	 */
+	return 0.;
 }
 
-void destroy(LoadableElem* pEl)
+static void
+destroy(LoadableElem* pEl)
 {
-   DEBUGCOUTFNAME("destroy");
-   module_template* p = (module_template *)pEl->pGetData();
-   SAFEDELETE(p, EMmm);
+	DEBUGCOUTFNAME("destroy");
+
+	module_template* p = (module_template *)pEl->pGetData();
+
+	/*
+	 * delete private data
+	 */
+	
+	SAFEDELETE(p, EMmm);
 }
+
+static struct
+LoadableCalls lc = {
+	read,
+	i_get_num_dof,
+	set_dof,
+	output,
+	restart,
+	work_space_dim,
+	ass_jac,
+	ass_eig,
+	ass_res,
+	before_predict,
+	after_predict,
+	update,
+	i_get_initial_num_dof,
+	initial_work_space_dim,
+	initial_ass_jac,
+	initial_ass_res,
+	set_value,
+	set_initial_value,
+	i_get_num_priv_data,
+	d_get_priv_data,
+	destroy
+};
+
+extern "C" {
+	void *calls = &lc;
+}
+
