87f12612bb39693c30f6782c73b32f165b0fe8c6
masarati <>
2001-05-17 08:20:06 +0000

    abbozzo di plugin per script in tcl

---
+++ mbdyn/libraries/libmbutil/tclpgin.h
@@ -0,0 +1,59 @@
+/* 
+ * MBDyn (C) is a multibody analysis code. 
+ * http://www.mbdyn.org
+ *
+ * Copyright (C) 1996-2000
+ *
+ * Pierangelo Masarati	<masarati@aero.polimi.it>
+ * Paolo Mantegazza	<mantegazza@aero.polimi.it>
+ *
+ * Dipartimento di Ingegneria Aerospaziale - Politecnico di Milano
+ * via La Masa, 34 - 20156 Milano, Italy
+ * http://www.aero.polimi.it
+ *
+ * Changing this copyright notice is forbidden.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef TCLPGIN_H
+#define TCLPGIN_H
+
+#ifdef WITH_TCL
+
+#include <tcl.h>
+#include <mathp.h>
+
+class TclPlugIn : public MathParser::PlugIn {
+protected:
+	Tcl_Interp *interp;
+	Tcl_Obj *cmd;
+
+public:
+	TclPlugIn(MathParser& mp);
+	~TclPlugIn(void);
+	const char *sName(void) const;
+	int Read(int argc, char *argv[]);
+
+	TypedValue GetVal(void) const;
+};
+
+extern MathParser::PlugIn *
+tcl_plugin(MathParser& mp, void *arg);
+
+#endif /* WITH_TCL */
+
+#endif /* TCLPGIN_H */
+
