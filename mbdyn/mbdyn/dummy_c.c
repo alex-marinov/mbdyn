922dae1369c2ddb7278ef91dccdd19827b46b983
masarati <>
2001-05-18 11:50:07 +0000

    ora matmo funziona; occorre lavorare sui dummy_*

---
+++ mbdyn/mbdyn/dummy_c.c
@@ -0,0 +1,8 @@
+extern double pow_dd(double,int);
+
+int
+dummy(void)
+{
+	double d;
+	return pow_dd(d,1);
+}
---
+++ mbdyn/mbdyn/dummy_f.f
@@ -0,0 +1,7 @@
+      subroutine dummy
+      integer i
+      double precision d
+      read(1,*)i
+      write(1,*)i
+      end
+
