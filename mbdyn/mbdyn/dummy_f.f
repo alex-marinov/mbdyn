659750f30612b098b54e245da67b362b0c346617
masarati <>
2001-05-18 12:19:27 +0000

    sistemati meglio dummy_c; rivisto il module-matmo

--- mbdyn/mbdyn/dummy_f.f
+++ mbdyn/mbdyn/dummy_f.f
@@ -1,7 +1,44 @@
-      subroutine dummy
+C
+C MBDyn (C) is a multibody analysis code. 
+C http://www.mbdyn.org
+C  
+C Copyright (C) 1996-2000
+C    
+C Pierangelo Masarati  <masarati@aero.polimi.it>
+C Paolo Mantegazza     <mantegazza@aero.polimi.it>
+C
+C Dipartimento di Ingegneria Aerospaziale - Politecnico di Milano
+C via La Masa, 34 - 20156 Milano, Italy
+C http://www.aero.polimi.it
+C
+C Changing this copyright notice is forbidden.
+C
+C This program is free software; you can redistribute it and/or modify
+C it under the terms of the GNU General Public License as published by
+C the Free Software Foundation; either version 2 of the License, or
+C any later version.
+C
+C This program is distributed in the hope that it will be useful,
+C but WITHOUT ANY WARRANTY; without even the implied warranty of
+C MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+C GNU General Public License for more details.
+C
+C You should have received a copy of the GNU General Public License
+C along with this program; if not, write to the Free Software
+C Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+C
+C
+C Questa routine e' usata solo ed esclusivamente per caricare alcune
+C routines della libf2c in mbdyn, in modo che siano accessibili ai
+C moduli dinamici in fortran che le richiedono. In teoria non dovrebbe
+C essere necessaria se la libf2c viene caricata dinamicamente, comunque ...
+C
+      subroutine dummy_f
       integer i
       double precision d
+      
       read(1,*)i
       write(1,*)i
+
       end
 
