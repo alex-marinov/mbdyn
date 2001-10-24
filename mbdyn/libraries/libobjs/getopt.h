8e91447ee5db1214b48977fd1136c81a145deaf8
masarati <>
2001-10-24 17:44:31 +0000

    copyright

--- mbdyn/libraries/libobjs/getopt.h
+++ mbdyn/libraries/libobjs/getopt.h
@@ -37,7 +37,28 @@
  * implementation for those systems that do not have a native one.
  * The intellectual property of the OpenLDAP implementation remains 
  * with the original Authors, whose contribution is kindly appreciated.
+ *
+ * The original copyright statement follows.
  */
+
+/* Declarations for getopt.
+ * Copyright (C) 1989,90,91,92,93,94,96,97,98 Free Software Foundation, Inc.
+ * This file is part of the GNU C Library.
+ *
+ * The GNU C Library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * The GNU C Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with the GNU C Library; see the file COPYING.LIB.  If not,
+ * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.  */
 
 #ifndef GETOPT_H
 #define GETOPT_H
