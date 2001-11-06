013485db2b858c0d35bf2498eb0802c5c713a609
masarati <>
2001-11-06 11:02:46 +0000

    *** empty log message ***

--- mbdyn/check_cvs.sh
+++ mbdyn/check_cvs.sh
@@ -2,18 +2,18 @@
 
 cvs status -v 2>/dev/null | \
 awk '
-	/^File/ {
-		f = $2
-	} 
 	/Repository revision/ {
-		c = $3
+		c = $3;
+		f = $4;
 	} 
 	/MBDYN_REL_ENG/ {
-		if (match($3,"([.0-9]+)")) {
-			r = substr($3,RSTART,RLENGTH);
+		if (match($3, "([.0-9]+)")) {
+			r = substr($3, RSTART, RLENGTH);
 			if (c != r) {
-				printf("%s: %s -> %s\n",f,c,r)
+				printf("%s: %s -> %s\n", f, c, r)
 			}
 		}
-	}'
+	}' | \
+	sed "s;.*mbdyn-1\.0/;;" | \
+	sed "s;,v;;"
 
