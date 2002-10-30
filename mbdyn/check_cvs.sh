96a56f1f01068aabf73ac3b3cada5d0e7fa5dfdf
masarati <>
2002-10-30 18:19:15 +0000

    improve check

--- mbdyn/check_cvs.sh
+++ mbdyn/check_cvs.sh
@@ -6,14 +6,14 @@ awk '
 		c = $3;
 		f = $4;
 	} 
-	/MBDYN_REL_ENG/ {
+	$1=="MBDYN_REL_ENG" {
 		if (match($3, "([.0-9]+)")) {
 			r = substr($3, RSTART, RLENGTH);
 			if (c != r) {
-				printf("%s: %s -> %s\n", f, c, r)
+				printf("cvs diff -u -r %s -r %s %s\n", r, c, f);
 			}
 		}
 	}' | \
-	sed "s;.*mbdyn-1\.0/;;" | \
+	sed "s;/.*mbdyn-1\.0/;;" | \
 	sed "s;,v;;"
 
