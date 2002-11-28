df549e30d7ed758cb2b22272bacb150de037c7fc
masarati <>
2002-11-28 01:06:22 +0000

    many improvements ...

--- mbdyn/var/pod_octave.m
+++ mbdyn/var/pod_octave.m
@@ -1,54 +1,71 @@
-function [S,Aout,data,A,BB,mn] = pod(file, stp, ns, novel, dt)
+%[S, Aout, B, mn, scl, ee, vv, X] = pod_octave(A, ns, dt)
+%
+%input:
+%	A	data (number of frames x number of outputs)
+%	ns	desired POMs
+%	dt	time lag between two frames (optional, defaults to 1.0)
+%
+%output:
+%	S	singular values
+%	Aout	data after POD
+%	B	POMs
+%	mn	mean value of data
+%	scl	scale factor of data-mn
+%	ee	eigenvalues
+%	vv	eigenvectors
+%	X	physical eigenvectors
+%
+function [S, Aout, B, mn, scl, ee, vv, X] = pod_octave(A, ns, dt)
 
-if nargin < 5,
+if nargin < 3,
 	dt = 1.;
 end
 
-%%% review
-data = dlmread(file, ' ');
-[N, m] = size(data)
-if ( stp > N)
-    error('too many steps required');
-end
-if ( ns > m)
-    error('too many sv required');
-end
-if (novel == 1) 
-    A = [];
-    for i = 1:posgdl
-        A =[A, data(1:stp,[1+(i-1)*12, 2+(i-1)*12 , 3+(i-1)*12,   4+(i-1)*12, 5+(i-1)*12 , 6+(i-1)*12])];
-    end
-    A = [A, data(1:stp, 12*posgdl+1:end)];
-else
-    A = data(1:stp, :);
+[r, c] = size(A);
+
+if nargin < 2,
+	ns = c;
+else 
+	if ( ns > c),
+  	  error('too many sv required');
+	end
 end
 
-[r,c] = size(A);
-for i=1:c
-    mn(i) = mean(A(:,i));
-    A(:,i) = A(:,i)-mn(i);
-    scl(i) = max(abs(A(:,i)));
-    if (scl(i) ~= 0)
-        A(:,i) = A(:,i)/scl(i);
+% detrend and normalize
+for i = 1:c,
+    mn(i) = mean(A(:, i));
+    A(:, i) = A(:, i)-mn(i);
+    scl(i) = max(abs(A(:, i)));
+    if (scl(i) > 1.e-9),
+        A(:, i) = A(:, i)/scl(i);
+    else
+	A(:, i) = zeros(r, 1);
+	disp(sprintf("dof %d: output is negligible", i));
     end
 end
 
-%% This is the big octave drawback: no eigs()
-[Utmp,Etmp] = eig(A*A');
-[Etmp,I] = sort(diag(Etmp));
+%%% This is the big octave drawback: no eigs() ...
+[Utmp, Etmp] = eig(A*A');
+Etmp = diag(Etmp);
+[Etmp2, I] = sort(Etmp);
 E = Etmp(I(r:-1:r-ns+1));
-U = Utmp(:,I(r:-1:r-ns+1));
+U = Utmp(:, I(r:-1:r-ns+1));
 
-B =U'*A;
-for i = 1:ns
-    S(i) = norm(B(i,:));
-    BB(i,:) = B(i,:)/S(i)^2;
+B = U'*A;
+for i = 1:ns,
+    S(i) = norm(B(i, :));	% S = sqrt(E)
+    B(i, :) = B(i, :)/S(i)^2;
 end
-Aout = BB*A';
+% Aout = B*A';			% = E^-1*U'*A*A' = E^-1*U'*U*E*U' = U'
+Aout = U';
 
-H = ((Aout(:, 1:r-1)')\(Aout(:, 2:r)'))'
+%%% This is a very rough estimate of the transition matrix ...
+H = ((Aout(:, 1:r-1)')\(Aout(:, 2:r)'))';
+
+% physical eigenvalues and eigenvectors ...
 [vv, eetmp] = eig(H);
 ee = diag(eetmp)/dt;
+X = vv'*B;
 
 % eigenvectors:
-% plot(([1 1;sqrt(-1) -sqrt(-1)]*vv(:,[6 7])'*BB(1:12,3:12:108))')
+% plot(([1 1;sqrt(-1) -sqrt(-1)]*X([6 7], l+1))')
