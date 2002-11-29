0189f5b8ff8e7a390ca4ab8bb5a9ebe50f22d413
masarati <>
2002-11-29 00:55:05 +0000

    fix pod_read

--- mbdyn/var/pod_read.m
+++ mbdyn/var/pod_read.m
@@ -1,15 +1,15 @@
-%[W, cnt] = pod_read( filename [ , nstep ] )
+%[W, cnt] = pod_read( filename [ , ndof ] )
 %
 %input:
 %	filename	name of the input file
-%	nstep		number of time steps (optional)
+%	ndof		number of dofs (optional)
 %
 %output:
-%	W		data (array if nstep is not defined, else a matrix
+%	W		data (array if ndof is not defined, else a matrix
 %			nstep x ndofs)
 %	cnt		number of values read
 %
-function [W, cnt] = pod_read(filename, nstep)
+function [W, cnt] = pod_read(filename, ndof)
 
 [fid, msg] = fopen(filename, "r", "native");
 if (fid == -1),
@@ -19,8 +19,10 @@ end
 if nargin < 2,
 	[W, cnt] = fread(fid, Inf, "double", 0, "native");
 else
-	[W, cnt] = fread(fid, [nstep, Inf], "double", 0, "native");
+	[W, cnt] = fread(fid, [ndof, Inf], "double", 0, "native");
 end
 
 fclose(fid);
+
+W = W';
 
