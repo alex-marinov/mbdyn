4c48493a9c170190bf995151f86edc2e19ce0241
masarati <>
2004-01-02 08:31:19 +0000

    fix inuse build and add more features

--- mbdyn/mbdyn/base/inuse.cc
+++ mbdyn/mbdyn/base/inuse.cc
@@ -29,17 +29,16 @@
  */
 
 #ifdef HAVE_CONFIG_H
-#include <mbconfig.h>           /* This goes first in every *.c,*.cc file */
+#include "mbconfig.h"           /* This goes first in every *.c,*.cc file */
 #endif /* HAVE_CONFIG_H */
 
 #include <stdlib.h>
-#include <ac/iostream>
+#include "ac/iostream"
 
 #ifdef USE_MULTITHREAD
 
 #include <unistd.h>
-#include <pthread.h>
-#include <semaphore.h>
+#include "ac/pthread.h"		/* includes POSIX semaphores */
 
 #include "mbconfig.h"
 #include "veciter.h"
@@ -49,20 +48,34 @@ unsigned rst = 10;
 
 class A : public InUse {
 private:
+	unsigned label;
 	unsigned who;
 public:
-	A(void) {};
+	A(unsigned l) : label(l) {};
 	virtual ~A(void) {};
 
-	void Set(unsigned w) { who = w; usleep(dst + rand()%rst); };
+	unsigned Label(void) const { return label; };
+	void Set(unsigned w) {
+		who = w;
+
+		unsigned s = dst;
+		if (rst) {
+			s += rand()%rst;
+		}
+		if (s) {
+			usleep(s);
+		}
+	};
 	unsigned Get(void) const { return who; };
 };
 
 struct Arg {
 	unsigned n;
+	unsigned cnt;
+	bool stop;
 	pthread_t t;
 	sem_t s;
-	VecIter<A *> i;
+	MT_VecIter<A *> i;
 	unsigned *c;
 };
 
@@ -70,14 +83,17 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
 static void
-f2(VecIter<A *>* pi, unsigned n)
+f2(VecIter<A *> &i, unsigned n, unsigned &cnt)
 {
 	A* pA = NULL;
 
-	if (pi->bGetFirst(pA)) {
+	cnt = 0;
+
+	if (i.bGetFirst(pA)) {
 		do {
 			pA->Set(n);
-		} while (pi->bGetNext(pA));
+			cnt++;
+		} while (i.bGetNext(pA));
 	}
 }
 
@@ -86,16 +102,24 @@ f(void *p)
 {
 	Arg *arg = (Arg *)p;
 
-	sem_wait(&arg->s);
+	while (true) {
+		sem_wait(&arg->s);
 
-	f2(&arg->i, arg->n);
+		if (arg->stop) {
+			break;
+		}
 
-	pthread_mutex_lock(&mutex);
-	--*(arg->c);
-	if (*arg->c == 0) {
-		pthread_cond_signal(&cond);
+		f2(arg->i, arg->n, arg->cnt);
+
+		pthread_mutex_lock(&mutex);
+		--*(arg->c);
+		silent_cout("f: count " << *arg->c + 1 << " => " << *arg->c << std::endl);
+		if (*arg->c == 0) {
+			silent_cout("f: finished" << std::endl);
+			pthread_cond_signal(&cond);
+		}
+		pthread_mutex_unlock(&mutex);
 	}
-	pthread_mutex_unlock(&mutex);
 
 	return NULL;
 }
@@ -105,48 +129,75 @@ main(int argc, char* argv[])
 {
 	unsigned size = 1000;
 	unsigned nt = 1;
+	unsigned loops = 1;
 
-	if (argc > 1) {
-		size = atoi(argv[1]);
-		if (size <= 0) {
-			return -1;
+	if (argc == 1) {
+usage:;
+		char *s = strrchr(argv[0], '/');
+
+		if (s) {
+			s++;
+		} else {
+			s = argv[0];
 		}
-		
-		if (argc > 2) {
-			nt = atoi(argv[2]);
-			if (nt <= 0) {
-				return -1;
-			}
 
-			if (argc > 3) {
-				dst = atoi(argv[3]);
-				if (dst <= 0) {
-					return -1;
-				}
+		std::cout << "usage: " << s << " [lnsSt]" << std::endl
+			<< "\t-l <loops>" << std::endl
+			<< "\t-n <size>" << std::endl
+			<< "\t-s <sleeptime>" << std::endl
+			<< "\t-S <random sleeptime>" << std::endl
+			<< "\t-t <threads number>" << std::endl;
+		exit(EXIT_SUCCESS);
+	}
 
-				if (argc > 4) {
-					rst = atoi(argv[4]);
-					if (rst <= 0) {
-						return -1;
-					}
-				}
-			}
+	while (true) {
+		char	*next;
+		int	opt = getopt(argc, argv, "l:n:s:S:t:");
+
+		if (opt == EOF) {
+			break;
+		}
+
+		switch (opt) {
+		case 'l':
+			loops = strtoul(optarg, &next, 10);
+			break;
+
+		case 'n':
+			size = strtoul(optarg, &next, 10);
+			break;
+
+		case 's':
+			dst = strtoul(optarg, &next, 10);
+			break;
+
+		case 'S':
+			rst = strtoul(optarg, &next, 10);
+			break;
+
+		case 't':
+			nt = strtoul(optarg, &next, 10);
+			break;
+
+		default:
+			goto usage;
 		}
 	}
 
 	Arg arg[nt];
-	unsigned c = nt - 1;
+	unsigned c;
 
 	A** ppA = new A*[size];
 	for (unsigned i = 0; i < size; i++) {
-		ppA[i] = new A;
-		ppA[i]->SetInUse(false);
+		ppA[i] = new A(i);
 	}
 
 	for (unsigned i = 0; i < nt; i++) {
 		arg[i].n = i;
 		arg[i].i.Init(ppA, size);
 		arg[i].c = &c;
+		arg[i].cnt = 0;
+		arg[i].stop = false;
 
 		if (i == 0) continue;
 
@@ -154,32 +205,54 @@ main(int argc, char* argv[])
 		pthread_create(&arg[i].t, NULL, f, &arg[i]);
 	}
 
-	pthread_mutex_lock(&mutex);
+	for (unsigned k = 0; k < 10; k++) {
+		arg[0].i.ResetAccessData();
+		c = nt - 1;
+
+		for (unsigned i = 1; i < nt; i++) {
+			sem_post(&arg[i].s);
+		}
+
+		f2(arg[0].i, arg[0].n, arg[0].cnt);
+
+		if (nt > 1) {
+			pthread_mutex_lock(&mutex);
+			if (c) {
+				silent_cout("main: count " << c << std::endl);
+				pthread_cond_wait(&cond, &mutex);
+				silent_cout("main: wait is over; count" << c << std::endl);
+			}
+			pthread_mutex_unlock(&mutex);
+		}
+
+		c = arg[0].cnt;
+		fprintf(stderr, "cnt = {%u", arg[0].cnt);
+		for (unsigned i = 1; i < nt; i++) {
+			fprintf(stderr, ", %u", arg[i].cnt);
+			c += arg[i].cnt;
+		}
+		fprintf(stderr, "} = %u\n", c);
+
+		unsigned who[nt];
+		memset(who, 0, sizeof(who));
+		for (unsigned i = 0; i < size; i++) {
+			who[ppA[i]->Get()]++;
+		}
+	
+		c = who[0];
+		fprintf(stderr, "who = {%u", who[0]);
+		for (unsigned i = 1; i < nt; i++) {
+			fprintf(stderr, ", %u", who[i]);
+			c += who[i];
+		}
+		fprintf(stderr, "} = %u\n", c);
+	}
 
 	for (unsigned i = 1; i < nt; i++) {
+		arg[i].stop = true;
 		sem_post(&arg[i].s);
+		pthread_join(arg[i].t, NULL);
 	}
-
-	f2(&arg[0].i, arg[0].n);
-
-	if (nt > 1) {
-		pthread_cond_wait(&cond, &mutex);
-	}
-	pthread_mutex_unlock(&mutex);
-
-	unsigned who[nt];
-	memset(who, 0, sizeof(who));
-	for (unsigned i = 0; i < size; i++) {
-		who[ppA[i]->Get()]++;
-	}
-
-	c = who[0];
-	fprintf(stderr, "who = {%u", who[0]);
-	for (unsigned i = 1; i < nt; i++) {
-		fprintf(stderr, ", %u", who[i]);
-		c += who[i];
-	}
-	fprintf(stderr, "} = %u\n", c);
 
 	return 0;
 }
