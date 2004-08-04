33ac3b609488d464f3df74c9e3ba1d166815bb75
masarati <>
2004-08-04 12:57:38 +0000

    cleanup

--- mbdyn/contrib/SimulinkInterface/sfun_mbdyn_start.c
+++ mbdyn/contrib/SimulinkInterface/sfun_mbdyn_start.c
@@ -1,20 +1,52 @@
+/* 
+ * MBDyn (C) is a multibody analysis code. 
+ * http://www.mbdyn.org
+ *
+ * Copyright (C) 1996-2004
+ *
+ * Pierangelo Masarati	<masarati@aero.polimi.it>
+ * Paolo Mantegazza	<mantegazza@aero.polimi.it>
+ *
+ * Dipartimento di Ingegneria Aerospaziale - Politecnico di Milano
+ * via La Masa, 34 - 20156 Milano, Italy
+ * http://www.aero.polimi.it
+ *
+ * Changing this copyright notice is forbidden.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation (version 2 of the License).
+ * 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 /*
-COPYRIGHT (C) 2003  Michele Attolico (attolico@aero.polimi.it)
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
-*/
+ * COPYRIGHT (C) 2003-2004
+ *
+ * Michele Attolico <attolico@aero.polimi.it>
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
 
 #define S_FUNCTION_NAME  sfun_mbdyn_start
 #define S_FUNCTION_LEVEL 2
@@ -26,21 +58,22 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
 #include "simstruc.h"
 #include <sys/types.h>
 #include <unistd.h>
+#include <errno.h>
 #include <signal.h>
 
 
 #define N_OF_PARAM 7
 
 #define MBDYN_PATH_NAME_PARAM	ssGetSFcnParam(S,0)
-#define MAX_MBDYN_PATH_NAME_DIM 200
+#define MAX_MBDYN_PATH_NAME_DIM 256
 #define MBDYN_PATH_PARAM	ssGetSFcnParam(S,1)
 #define MBDYN_PATH		((uint_T) mxGetPr(MBDYN_PATH_PARAM)[0])
 
 #define FILE_NAME_PARAM		ssGetSFcnParam(S,2)
-#define MAX_FILE_NEME_DIM	100
+#define MAX_FILE_NAME_DIM	256
 
 #define FILE_OUTPUT_PARAM	ssGetSFcnParam(S,3)
-#define MAX_FILE_OUTPUT_DIM	20
+#define MAX_FILE_OUTPUT_DIM	256
 #define OUTPUT_PARAM		ssGetSFcnParam(S,4)
 #define OUTPUT			((uint_T) mxGetPr(OUTPUT_PARAM)[0])
 
@@ -51,118 +84,134 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
 #define PEDANTIC		((uint_T) mxGetPr(PEDANTIC_PARAM)[0])
 
 
-static void mdlInitializeSizes(SimStruct *S)
+static void
+mdlInitializeSizes(SimStruct *S)
 {
+	/* Number of expected parameters */
+	ssSetNumSFcnParams(S, N_OF_PARAM);
+	if (ssGetNumSFcnParams(S) != ssGetSFcnParamsCount(S)) {
+		/* Return if # of expected != # of actual parameters */
+		printf("S-function params %d do not match expected %d\n",
+				ssGetNumSFcnParams(S), ssGetSFcnParamsCount(S));
+		return;
+	}
 
-  	ssSetNumSFcnParams(S, N_OF_PARAM);  /* Number of expected parameters */
-    if (ssGetNumSFcnParams(S) != ssGetSFcnParamsCount(S)) {
-        /* Return if number of expected != number of actual parameters */
-        return;
-    }
+	ssSetNumContStates(S, 0);
+	ssSetNumDiscStates(S, 0);
 
-    ssSetNumContStates(S, 0);
-    ssSetNumDiscStates(S, 0);
-
-    if (!ssSetNumInputPorts(S, 0)) return;
-    /*
-     * Set direct feedthrough flag (1=yes, 0=no).
-     * A port has direct feedthrough if the input is used in either
-     * the mdlOutputs or mdlGetTimeOfNextVarHit functions.
-     * See matlabroot/simulink/src/sfuntmpl_directfeed.txt.
-     */
+	if (!ssSetNumInputPorts(S, 0)) {
+		printf("No input ports defined\n");
+		return;
+	}
+	
+	/*
+	 * Set direct feedthrough flag (1=yes, 0=no).
+	 * A port has direct feedthrough if the input is used in either
+	 * the mdlOutputs or mdlGetTimeOfNextVarHit functions.
+	 * See matlabroot/simulink/src/sfuntmpl_directfeed.txt.
+	 */
     
-    if (!ssSetNumOutputPorts(S, 0)) return;
+	if (!ssSetNumOutputPorts(S, 0)) {
+		printf("No output ports defined\n");
+		return;
+	}
  
-    ssSetNumSampleTimes(S, 1);
-    ssSetNumRWork(S, 0);
-    ssSetNumIWork(S, 1);
-    ssSetNumPWork(S, 0);
-    ssSetNumModes(S, 0);
-    ssSetNumNonsampledZCs(S, 0);
+	ssSetNumSampleTimes(S, 1);
+	ssSetNumRWork(S, 0);
+	ssSetNumIWork(S, 1);
+	ssSetNumPWork(S, 0);
+	ssSetNumModes(S, 0);
+	ssSetNumNonsampledZCs(S, 0);
 
-    ssSetOptions(S, 0);
+	ssSetOptions(S, 0);
 }
 
-static void mdlInitializeSampleTimes(SimStruct *S)
+static void
+mdlInitializeSampleTimes(SimStruct *S)
 {
-    ssSetSampleTime(S, 0, CONTINUOUS_SAMPLE_TIME);
-    ssSetOffsetTime(S, 0, 0.0);
-
+	ssSetSampleTime(S, 0, CONTINUOUS_SAMPLE_TIME);
+	ssSetOffsetTime(S, 0, 0.0);
 }
-
 
 #define MDL_START 
 #if defined(MDL_START)
-static void mdlStart(SimStruct *S)
+static void
+mdlStart(SimStruct *S)
 {
 #ifdef MATLAB_MEX_FILE
 	int pid;
-	printf("Qui!\n");
-	switch(pid = fork()) {
-	case 0 :{
+
+	pid = fork();
+
+	switch (pid) {
+	case 0 :
+	{
 		int count = 0;
-		char *parameter[8];
-		char * mbdyn_name;
-		char * file_output;
-		char file_name[MAX_FILE_NEME_DIM];
+		char *parameter[8] = { '\0' };
+		char mbdyn_name[MAX_MBDYN_PATH_NAME_DIM] = { '\0' };
+		char file_output[MAX_FILE_OUTPUT_DIM] = { '\0' };
+		char file_name[MAX_FILE_NAME_DIM] = { '\0' };
 		char file_option[] = "-f";
 		char output_option[] = "-o";
 		char verbose_option[] = "-ss";
 		char pedantic_option[] = "-P";
 
-		fprintf(stderr,"qui\n");
-
-		if(MBDYN_PATH) {
-			mbdyn_name = calloc(MAX_MBDYN_PATH_NAME_DIM, sizeof(char));
-			mxGetString(MBDYN_PATH_NAME_PARAM, mbdyn_name, MAX_MBDYN_PATH_NAME_DIM);	
-			parameter[count] = mbdyn_name;
-			count ++;	
+		if (MBDYN_PATH) {
+			mxGetString(MBDYN_PATH_NAME_PARAM, mbdyn_name,
+					sizeof(mbdyn_name));
 		} else {
-			mbdyn_name= calloc(8, sizeof(char));
-			memcpy(mbdyn_name,"./mbdyn\0",8);
-			parameter[count] = mbdyn_name;
-			count ++;
+			strcpy(mbdyn_name, "./mbdyn");
 		}
-		/*char mbdyn_name [] ="../mbdyn-devel1/mbdyn/mbdyn";
-		count ++;*/
-		mxGetString(FILE_NAME_PARAM, file_name, MAX_FILE_NEME_DIM);
+		parameter[count] = mbdyn_name;
+		count++;	
+
+		mxGetString(FILE_NAME_PARAM, file_name, sizeof(file_name));
 		parameter[count] = file_option;
-		count ++;		
+		count++;		
 		parameter[count] = file_name;
-		count ++;
+		count++;
 		
-		if(OUTPUT) {
-			file_output=calloc(MAX_FILE_OUTPUT_DIM, sizeof(char));
-			mxGetString(FILE_OUTPUT_PARAM, file_output, MAX_FILE_OUTPUT_DIM);	
+		if (OUTPUT) {
 			parameter[count] = output_option;
-			count ++;
+			count++;
+			mxGetString(FILE_OUTPUT_PARAM, file_output,
+					sizeof(file_output));
 			parameter[count] = file_output;
-			count ++;
+			count++;
 		}
 		
-		if(!VERBOSE) {
+		if (!VERBOSE) {
 			parameter[count] = verbose_option;
-			count ++;				
+			count++;				
 		}
 		
-		if(PEDANTIC) {
+		if (PEDANTIC) {
 			parameter[count] = pedantic_option;
-			count ++;				
+			count++;				
 		}
 		parameter[count] = NULL;
 		
-		if(execv(mbdyn_name, parameter) == -1) {
+		if (execv(mbdyn_name, parameter) == -1) {
 			fprintf(stderr,"Cannot start child process\n");
 			exit(-1);
 		}
 	}
-	case -1: 
-		fprintf(stderr,"Cannot fork\n");
+	
+	case -1:
+	{
+		char	errbuf[256];
+
+		if (strerror_r(errno, errbuf, sizeof(errbuf)) == 0) {
+			fprintf(stderr,"fork failed: %s\n", errbuf);
+		} else {
+			fprintf(stderr,"fork failed, and strerror_r too\n");
+		}
 		break;
+	}
 
 	default:
-		{
-		int sleeptime = 1;
+	{
+		int sleeptime = 0;
 		printf("Simulink start MBDyn task\n", sleeptime);
 		if (sleeptime) {
 			printf("\tsleeping %d s\n", sleeptime);
@@ -171,15 +220,18 @@ static void mdlStart(SimStruct *S)
 			}
 		}
 		break;
-		}
 	}
+	}
+
 	ssGetIWork(S)[0] = (int_T)pid;
 #endif /*MATLAB_MEX_FILE*/
 }
 #endif /*MDL_START*/
 
-static void mdlOutputs(SimStruct *S, int_T tid)
+static void
+mdlOutputs(SimStruct *S, int_T tid)
 {
+	;
 }
 
 
@@ -187,9 +239,6 @@ static void mdlTerminate(SimStruct *S)
 {
 #ifdef MATLAB_MEX_FILE
 	int pid = (int_T)ssGetIWork(S)[0];
-	printf("Qui!\n");
-	printf("Qui2!\n");
-	/*kill(pid,SIGINT);*/
 #endif /*MATLAB_MEX_FILE*/
 }
 
