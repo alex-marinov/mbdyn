5de53dfd961b79c798f2a3486150f5e6cd145a7b
masarati <>
2004-08-16 11:05:44 +0000

    make install.* as well

--- mbdyn/manual/make.sh
+++ mbdyn/manual/make.sh
@@ -8,69 +8,72 @@ if [ -r $VERSIONFILE ] ; then
 	VV="-$VERSION"
 fi
 LOG="make.log"
-IN="input"
-OUT="mbdyn-input$VV"
 
-rm -rf "$IN" "$OUT" "$LOG"
+TGT="input install"
+for IN in $TGT; do
+	OUT="mbdyn-${IN}$VV"
 
-echo "making dvi..."
-which latex > /dev/null 2>&1
-RC=$?
-if [ "$RC" != 0 ] ; then
-	echo "unable to find latex"
-	exit 1
-fi
-latex "$IN" >> "$LOG" 2>&1
-which bibtex > /dev/null 2>&1
-RC=$?
-if [ "$RC" = 0 ] ; then
-	bibtex "$IN" >> "$LOG" 2>&1
-else
-	echo "warning: no bibtex available..."
-fi
-latex "$IN" >> "$LOG" 2>&1
-latex "$IN" >> "$LOG" 2>&1
+	rm -rf "$IN" "$OUT" "$LOG"
 
-echo "making ps..."
-which dvips > /dev/null 2>&1
-RC=$?
-if [ "$RC" != 0 ] ; then
-	echo "unable to find dvips..."
-else
-	dvips -o "$OUT.ps" "$IN" >> "$LOG" 2>&1
-	
-	echo "making pdf..."
-	which ps2pdf > /dev/null 2>&1
+	echo "making $OUT.dvi..."
+	which latex > /dev/null 2>&1
 	RC=$?
-	CONVERT=""
+	if [ "$RC" != 0 ] ; then
+		echo "unable to find latex"
+		exit 1
+	fi
+	latex "$IN" >> "$LOG" 2>&1
+	which bibtex > /dev/null 2>&1
+	RC=$?
 	if [ "$RC" = 0 ] ; then
-		CONVERT="ps2pdf"
+		bibtex "$IN" >> "$LOG" 2>&1
 	else
-		which convert > /dev/null 2>&1
+		echo "warning: no bibtex available..."
+	fi
+	latex "$IN" >> "$LOG" 2>&1
+	latex "$IN" >> "$LOG" 2>&1
+
+	echo "making $OUT.ps..."
+	which dvips > /dev/null 2>&1
+	RC=$?
+	if [ "$RC" != 0 ] ; then
+		echo "unable to find dvips..."
+	else
+		dvips -o "$OUT.ps" "$IN" >> "$LOG" 2>&1
+	
+		echo "making $OUT.pdf..."
+		which ps2pdf > /dev/null 2>&1
 		RC=$?
+		CONVERT=""
 		if [ "$RC" = 0 ] ; then
-			CONVERT="convert"
+			CONVERT="ps2pdf"
 		else
-			echo "unable to find either ps2pdf or convert"
+			which convert > /dev/null 2>&1
+			RC=$?
+			if [ "$RC" = 0 ] ; then
+				CONVERT="convert"
+			else
+				echo "unable to find either ps2pdf or convert"
+			fi
+		fi
+		if [ "$CONVERT" != "" ] ; then
+			"$CONVERT" "$OUT.ps" "$OUT.pdf" >> "$LOG" 2>&1
 		fi
 	fi
-	if [ "$CONVERT" != "" ] ; then
-		"$CONVERT" "$OUT.ps" "$OUT.pdf" >> "$LOG" 2>&1
-	fi
-fi
 
-echo "making html..."
-which latex2html > /dev/null 2>&1
-RC=$?
-if [ "$RC" != 0 ] ; then
-	echo "unable to find latex2html"
-else
-	latex2html \
-		-split 4 \
-		-toc_depth 3 \
-		-local_icons \
-		-address "<a href=\"http://www.aero.polimi.it/~mbdyn\">MBDyn</a>: MultiBody Dynamics Software<br>Document version: $VERSION <br>Last update: $DATE <br>Maintained by <a href=\"mailto:mbdyn@aero.polimi.it\">mbdyn@aero.polimi.it</a>" \
-		"$IN" >> "$LOG" 2>&1
-	mv -f "$IN" "$OUT"
-fi
+	echo "making $OUT.html..."
+	which latex2html > /dev/null 2>&1
+	RC=$?
+	if [ "$RC" != 0 ] ; then
+		echo "unable to find latex2html"
+	else
+		latex2html \
+			-split 4 \
+			-toc_depth 3 \
+			-local_icons \
+			-address "<a href=\"http://www.aero.polimi.it/~mbdyn\">MBDyn</a>: MultiBody Dynamics Software<br>Document version: $VERSION <br>Last update: $DATE <br>Maintained by <a href=\"mailto:mbdyn@aero.polimi.it\">mbdyn@aero.polimi.it</a>" \
+			"$IN" >> "$LOG" 2>&1
+		mv -f "$IN" "$OUT"
+	fi
+done
 
