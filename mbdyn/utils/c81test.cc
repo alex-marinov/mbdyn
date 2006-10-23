516df751314c4d69d1db4b4362eb1a7eea7d798f
masarati <>
2006-10-23 20:24:18 +0000

    make it more useful

--- mbdyn/utils/c81test.cc
+++ mbdyn/utils/c81test.cc
@@ -46,6 +46,25 @@
 #include <c81data.h>
 #endif /* USE_AERODYNAMIC_ELEMS */
 
+static void
+usage(int rc)
+{
+	std::cerr <<
+"usage: testc81 [<options>] <file> [<alpha (deg)> <mach>]\n"
+"\n"
+"options:\n"
+"\t-a <alpha>\t"	"dump coefficients for incidence <alpha>\n"
+"\t-c\t\t"		"interpret <file> as traditional c81 format (default)\n"
+"\t-C <coef>\t"		"only dump coefficient <coef> (bust be cl, cd or cm)\n"
+"\t-d [<dump>]\t"	"dump contents (to optional file <dump>, if given)\n"
+"\t-f\t\t"		"interpret <file> as free format\n"
+"\t-m <mach>\t"		"dump coefficients for Mach number <mach>\n"
+"\t-o\t\t"		"interpret <file> as fc511 format\n"
+		<< std::endl;
+
+	exit(rc);
+}
+
 /*
  * uso: testc81 file alpha mach
  */
@@ -53,20 +72,52 @@ int
 main(int argc, char *argv[])
 {
 #if defined(USE_AERODYNAMIC_ELEMS)
-	char	mode = 'c';
-	char	*dump_fname = NULL;
-	bool	dump(false);
+	char		mode = 'c';
+	char		*dump_fname = NULL;
+	bool		dump(false);
+	doublereal	alpha;
+	doublereal	mach;
+	enum {
+		GOT_NONE	= 0x0000U,
+		GOT_ALPHA	= 0x0001U,
+		GOT_MACH	= 0x0002U,
+		GOT_ALPHAMACH	= (GOT_ALPHA|GOT_MACH)
+	};
+	unsigned	got = GOT_NONE;
+	char		coef = '\0';
 	
-	while (1) {
-		int opt = getopt(argc, argv, "cd::fo");
+	while (true) {
+		int opt = getopt(argc, argv, "a:cC:d::fm:o");
 
 		if (opt == EOF) {
 			break;
 		}
 
 		switch (opt) {
+		case 'a':
+			alpha = atof(optarg);
+			got |= GOT_ALPHA;
+			break;
+
 		case 'c':
 			mode = 'c';
+			break;
+
+		case 'C':
+			if (strcasecmp(optarg, "cl") == 0) {
+				coef = 'l';
+
+			} else if (strcasecmp(optarg, "cd") == 0) {
+				coef = 'd';
+
+			} else if (strcasecmp(optarg, "cm") == 0) {
+				coef = 'm';
+
+			} else {
+				std::cerr << "unknown coefficient "
+					"\"" << optarg << "\"" << std::endl;
+				usage(EXIT_FAILURE);
+			}
 			break;
 
 		case 'd':
@@ -80,22 +131,25 @@ main(int argc, char *argv[])
 			mode = 'f';
 			break;
 
+		case 'm':
+			mach = atof(optarg);
+			got |= GOT_MACH;
+			break;
+
 		case 'o':
 			mode = 'o';
 			break;
 
 		default:
-			exit(EXIT_FAILURE);
+			usage(EXIT_FAILURE);
 		}
 	}
 
 	argv += optind;
 	argc -= optind;
 		
-	if (argc != 3 && !dump) {
-		std::cerr << "usage: testc81 [<options>] <file> [<alpha (deg)> <mach>]"
-			<< std::endl;
-		exit(EXIT_SUCCESS);
+	if (argc != 1) {
+		usage(EXIT_FAILURE);
 	}
 
 	std::ifstream in(argv[0]);
@@ -128,30 +182,114 @@ main(int argc, char *argv[])
 
 	if (rc) {
 		std::cerr << "unable to read c81 data from file "
-			"'" << argv[0] << "'" << std::endl;
+			"\"" << argv[0] << "\"" << std::endl;
 		exit(EXIT_FAILURE);
 	}
 
 	if (dump_fname) {
 		std::ofstream out(dump_fname);
 		write_c81_data_free_format(out, data);
+		exit(EXIT_SUCCESS);
 
 	} else if (dump) {
 		write_c81_data_free_format(std::cout, data);
+		exit(EXIT_SUCCESS);
 	}
 
-	if (argc == 3) {
-		doublereal alpha(atof(argv[1])), mach(atof(argv[2]));
+	if ((got & GOT_ALPHAMACH) == GOT_ALPHAMACH) {
+		switch (coef) {
+		case 'l':
+			std::cout << get_c81_coef(data->NML, data->ml,
+				data->NAL, data->al, alpha, mach)
+				<< std::endl;
+			break;
 
-		std::cout 
-			<< "alpha: " << alpha << std::endl
-			<< "mach: " << mach << std::endl
-			<< "cl: " << get_c81_coef(data->NML, data->ml, data->NAL, 
-					data->al, alpha, mach) << std::endl
-			<< "cd: " << get_c81_coef(data->NMD, data->md, data->NAD, 
-					data->ad, alpha, mach) << std::endl
-			<< "cm: " << get_c81_coef(data->NMM, data->mm, data->NAM, 
-					data->am, alpha, mach) << std::endl;
+		case 'd':
+			std::cout << get_c81_coef(data->NMD, data->md,
+				data->NAD, data->ad, alpha, mach)
+				<< std::endl;
+			break;
+
+		case 'm':
+			std::cout << get_c81_coef(data->NMM, data->mm,
+				data->NAM, data->am, alpha, mach)
+				<< std::endl;
+			break;
+
+		case '\0':
+			std::cout 
+				<< "alpha: " << alpha << std::endl
+				<< "mach: " << mach << std::endl
+				<< "cl: " << get_c81_coef(data->NML, data->ml,
+					data->NAL, data->al, alpha, mach)
+					<< std::endl
+				<< "cd: " << get_c81_coef(data->NMD, data->md,
+					data->NAD, data->ad, alpha, mach)
+					<< std::endl
+				<< "cm: " << get_c81_coef(data->NMM, data->mm,
+					data->NAM, data->am, alpha, mach)
+					<< std::endl;
+			break;
+		}
+
+	} else if (got & GOT_ALPHAMACH) {
+		int NM, NA;
+		double *m = 0, *a = 0;
+
+		switch (coef) {
+		case 'l':
+			NM = data->NML;
+			NA = data->NAL;
+			m = data->ml;
+			a = data->al;
+			break;
+
+		case 'd':
+			NM = data->NMD;
+			NA = data->NAD;
+			m = data->md;
+			a = data->ad;
+			break;
+
+		case 'm':
+			NM = data->NMM;
+			NA = data->NAM;
+			m = data->mm;
+			a = data->am;
+			break;
+
+		case '\0':
+			/* TODO */
+			break;
+		}
+
+		if (m) {
+			switch (got & GOT_ALPHAMACH) {
+			case GOT_ALPHA:
+				for (int i = 0; i < NM; i++) {
+					std::cout
+						<< m[i] << " "
+						<< get_c81_coef(NM, m,
+						NA, a, alpha, m[i])
+						<< std::endl;
+				}
+				break;
+
+			case GOT_MACH:
+				for (int i = 0; i < NA; i++) {
+					std::cout
+						<< a[i] << " "
+						<< get_c81_coef(NM, m,
+						NA, a, a[i], mach)
+						<< std::endl;
+				}
+				break;
+			}
+		}
+
+	} else {
+		std::cerr << "don't know what to do..." << std::endl;
+		usage(EXIT_FAILURE);
 	}
 
 #else /* !USE_AERODYNAMIC_ELEMS */
