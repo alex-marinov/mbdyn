ade8b071fdeb15eb5fc4feb45a4ed70abf03466e
morandini <>
2009-01-08 14:37:49 +0000

    copyright

--- mbdyn/include/ac/spinlock.h
+++ mbdyn/include/ac/spinlock.h
@@ -28,7 +28,12 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
+/*
+ * Copyright (C) 2009
+ *
+ * Marco Morandini
+ *
+ */
 #ifndef ac_spinlock_h
 #define ac_spinlock_h
 
