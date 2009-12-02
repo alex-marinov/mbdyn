c6d1b0b292b025e9ea6385a4f9621b82eafc164f
morandini <>
2009-12-02 08:09:20 +0000

    comment out unportable debug instructions

--- mbdyn/mbdyn/struct/screwjoint.cc
+++ mbdyn/mbdyn/struct/screwjoint.cc
@@ -39,15 +39,15 @@
 #include "Rot.hh"
 #include "hint_impl.h"
 
-#define _GNU_SOURCE 1
-#include <fenv.h>
-static void __attribute__ ((constructor))
-trapfpe ()
-{
-  /* Enable some exceptions.  At startup all exceptions are masked.  */
-
-  feenableexcept (FE_INVALID|FE_DIVBYZERO|FE_OVERFLOW);
-}
+// #define _GNU_SOURCE 1
+// #include <fenv.h>
+// static void __attribute__ ((constructor))
+// trapfpe ()
+// {
+//   /* Enable some exceptions.  At startup all exceptions are masked.  */
+// 
+//   feenableexcept (FE_INVALID|FE_DIVBYZERO|FE_OVERFLOW);
+// }
 
 /* ScrewJoint - begin */
 
