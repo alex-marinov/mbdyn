887b31bb4e41a89eac8c9f4f4881d9018d58cc48
masarati <>
2010-11-21 23:28:33 +0000

    first real round of interface

--- mbdyn/modules/module-charm/module-charm.cc
+++ mbdyn/modules/module-charm/module-charm.cc
@@ -54,14 +54,98 @@
 #include "dataman.h"
 #include "userelem.h"
 #include "indvel.h"
+#include "drive_.h"
 
 // CHARM's header file (apparently it is experimental)
 #undef __stdcall
 #define __stdcall
-#include "CharmWP.h"
+#include <CharmWP.h>
+
+#define MBCHARM_FAKE 1
+#ifdef MBCHARM_FAKE
+int
+allocWPAircraft(
+	int num_rotor_surf,
+	int *num_blades,
+	int *num_control_pts,
+	wpAircraft *aircraft)
+{
+	ASSERT(num_rotor_surf > 0);
+	ASSERT(num_blades != 0);
+	ASSERT(num_control_pts != 0);
+	ASSERT(aircraft != 0);
+
+	aircraft->number_rotors_or_surfaces = num_rotor_surf;
+
+	for (int ir = 0; ir < num_rotor_surf; ir++) {
+		wpRotorSurface *rotor = &aircraft->rotors[ir];
+
+		ASSERT(num_blades[ir] > 0);
+		ASSERT(num_control_pts[ir] > 0);
+
+		rotor->number_blades = num_blades[ir];
+		rotor->number_control_points = num_control_pts[ir];
+	}
+
+	return 0;
+}
+
+void
+initWPModule( 
+	int num_aircraft, 
+	wpAircraft *aircraft, 
+	int num_offrotor, 
+	int max_offrotor, 
+	chOffRotorEval *eval, 
+	int areEvalPointsFixed, 
+	chGlobal *global, 
+	wpOptions *options)
+{
+	ASSERT(num_aircraft == 1);
+	ASSERT(aircraft != 0);
+	ASSERT((num_offrotor == 0) || (eval != 0));
+	ASSERT(global != 0);
+	ASSERT(options != 0);
+}
+
+void
+updateIndVelocity(wpAircraft *aircraft, chOffRotorEval *eval)
+{
+	ASSERT(aircraft != 0);
+	ASSERT(eval != 0);
+}
+
+void
+setCHARMGlobal(chGlobal *global)
+{
+	ASSERT(global != 0);
+}
+
+void
+setWPAircraft(int index, wpAircraft *aircraft)
+{
+	ASSERT(aircraft != NULL);
+	ASSERT(index == 0);	// only one aircraft
+}
+
+void
+setCHARMOffRotor(int numPoints, chOffRotorEval *eval)
+{
+	ASSERT(eval != 0);
+}
+
+void
+updateWake(int isTrimming, chStatus *status)
+{
+	ASSERT(status != 0);
+}
+#endif // MBCHARM_FAKE
 
 class ModuleCHARM
-: virtual public Elem, public UserDefinedElem, public InducedVelocity {
+: virtual public Elem,
+	public UserDefinedElem,
+	public InducedVelocity
+{
 private:
 	// TODO: define per-point structure
 	struct PointData {
@@ -81,8 +165,22 @@ private:
 	typedef std::vector<PointData> PD;
 	PD::iterator m_data_iter;
 
+	DriveOwner m_Time;
+	std::vector<double> m_TrimTime;
+	std::vector<double>::const_iterator m_TrimTimeIter;
+
+	chGlobal m_chglobal;
+	wpOptions m_wpoptions;
+	std::vector<chOffRotorEval> m_eval_pts;
+
+	Mat3x3 m_Rh;
+	wpAircraft m_wpaircraft;
+
 	// add private data
 	int iFirstAssembly;
+
+	void Init_int(void);
+	void Update_int(void);
 
 public:
 	ModuleCHARM(unsigned uLabel, const DofOwner *pDO,
@@ -96,8 +194,10 @@ public:
 	virtual bool bSectionalForces(void) const;
 	virtual Vec3 GetInducedVelocity(const Vec3&) const;
 	virtual void AddSectionalForce(unsigned int uL,
-		const Vec3& F, const Vec3& M, doublereal dW, const Vec3& X);
+		const Vec3& F, const Vec3& M, doublereal dW,
+		const Vec3& X, const Mat3x3& R);
 
+	virtual void AfterPredict(VectorHandler& X, VectorHandler& XP);
 	virtual void Output(OutputHandler& OH) const;
 	virtual void WorkSpaceDim(integer* piNumRows, integer* piNumCols) const;
 	VariableSubMatrixHandler& 
@@ -132,6 +232,7 @@ ModuleCHARM::ModuleCHARM(
 : Elem(uLabel, flag(0)),
 UserDefinedElem(uLabel, pDO),
 InducedVelocity(uLabel, 0, 0, flag(0)),
+m_Time(new TimeDriveCaller(pDM->pGetDrvHdl())),
 iFirstAssembly(2)
 {
 	// help
@@ -158,8 +259,169 @@ iFirstAssembly(2)
 		}
 	}
 
-	// do something useful
+	// global data
+	m_chglobal.current_time = m_Time.dGet();
+	m_chglobal.trim_flag = 0;
+
+	// NOTE: ignore ground by now
+	m_chglobal.ground_flag = 0;
+
+	// NOTE: ignore filters
+	m_chglobal.iv_filter_order[0] = 0;
+	m_chglobal.iv_filter_order[1] = 0;
+#if 0
+	m_chglobal.iv_filter_freq[0];
+	m_chglobal.iv_filter_freq[1];
+	m_chglobal.iv_filter_damp[0];
+	m_chglobal.iv_filter_damp[1];
+#endif
+
+	// wake-panel options
+	m_wpoptions.kblind = 1;
+	m_wpoptions.klssim = 0;
+	m_wpoptions.kgmsim = 0;
+	m_wpoptions.kupdgam = 1;	// set to 0 when KTRSIM is set to 4
+	m_wpoptions.kshipsim = 0;
+	m_wpoptions.kc2sim = 0;
+	m_wpoptions.kc2lev = 0;
+	if (!HP.IsKeyWord("units")) {
+		silent_cerr("ModuleCHARM(" << uLabel << "): "
+			"\"units\" expected "
+			"at line " << HP.GetLineData() << std::endl);
+		throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+	}
+
+	if (HP.IsKeyWord("bu")) {
+		m_wpoptions.kunitsim = 0;	// 0: BU; 1: MKS
+
+	} else if (HP.IsKeyWord("si") || HP.IsKeyWord("mks")) {
+		m_wpoptions.kunitsim = 1;	// 0: BU; 1: MKS
+
+	} else {
+		silent_cerr("ModuleCHARM(" << uLabel << "): "
+			"unknown \"units\" "
+			"at line " << HP.GetLineData() << std::endl);
+		throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+	}
+
+	m_wpoptions.noutsim = 998;
+	if (HP.IsKeyWord("output" "unit")) {
+		m_wpoptions.noutsim = HP.GetInt();
+		if (m_wpoptions.noutsim <= 0) {
+			silent_cerr("ModuleCHARM(" << uLabel << "): "
+				"invalid output unit " << m_wpoptions.noutsim << " "
+				"at line " << HP.GetLineData() << std::endl);
+			throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+		}
+	}
+	m_wpoptions.nupsim = 0;
+
+
+	// aircraft
+	if (!HP.IsKeyWord("aircraft")) {
+		silent_cerr("ModuleCHARM(" << uLabel << "): "
+			"\"aircraft\" (aircraft node label) expected "
+			"at line " << HP.GetLineData() << std::endl);
+		throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+	}
 	pCraft = dynamic_cast<StructNode *>(pDM->ReadNode(HP, Node::STRUCTURAL));
+
+	ReferenceFrame RF(pCraft);
+	if (HP.IsKeyWord("orientation")) {
+		m_Rh = HP.GetRotRel(RF);
+	} else {
+		m_Rh = Eye3;
+	}
+
+	if (!HP.IsKeyWord("rotors")) {
+		silent_cerr("ModuleCHARM(" << uLabel << "): "
+			"\"rotors\" (number of rotors) expected "
+			"at line " << HP.GetLineData() << std::endl);
+		throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+	}
+	int num_rotors = HP.GetInt();
+	if (num_rotors < 1) {
+		silent_cerr("ModuleCHARM(" << uLabel << "): "
+			"invalid number of rotors " << num_rotors
+			<< " at line " << HP.GetLineData() << std::endl);
+		throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+	}
+
+	std::vector<int> v_num_blades(num_rotors);
+	std::vector<int> v_num_control_points(num_rotors);
+
+	for (int ir = 0; ir < num_rotors; ir++) {
+		v_num_blades[ir] = HP.GetInt();
+		if (v_num_blades[ir] < 1) {
+			silent_cerr("ModuleCHARM(" << uLabel << "): "
+				"invalid number of blades " << v_num_blades[ir]
+				<< " for rotor #" << ir << " of " << num_rotors
+				<< " at line " << HP.GetLineData() << std::endl);
+			throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+		}
+
+		v_num_control_points[ir] = HP.GetInt();
+		if (v_num_control_points[ir] < 1) {
+			silent_cerr("ModuleCHARM(" << uLabel << "): "
+				"invalid number of control points " << v_num_control_points[ir]
+				<< " for rotor #" << ir << " of " << num_rotors
+				<< " at line " << HP.GetLineData() << std::endl);
+			throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+		}
+	}
+
+	allocWPAircraft(num_rotors, &v_num_blades[0],
+           	&v_num_control_points[0], &m_wpaircraft);
+
+	if (m_wpaircraft.number_rotors_or_surfaces != num_rotors) {
+		silent_cerr("ModuleCHARM(" << uLabel << "): "
+			"number of rotors mismatch "
+			"(" << m_wpaircraft.number_rotors_or_surfaces
+			<< " instead of " << num_rotors << ") "
+			"at line " << HP.GetLineData() << std::endl);
+		throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+	}
+
+	for (int ir = 0; ir < num_rotors; ir++) {
+		wpRotorSurface *rotor = &m_wpaircraft.rotors[ir];
+		if (rotor->number_blades != v_num_blades[ir]) {
+			silent_cerr("ModuleCHARM(" << uLabel << "): "
+				"number of blades mismatch "
+				"(" << rotor->number_blades
+				<< " instead of " << v_num_blades[ir] << ") "
+				"for rotor #" << ir << " of " << num_rotors
+				<< " at line " << HP.GetLineData() << std::endl);
+			throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+		}
+		if (rotor->number_control_points != v_num_control_points[ir]) {
+			silent_cerr("ModuleCHARM(" << uLabel << "): "
+				"number of control points mismatch "
+				"(" << rotor->number_control_points
+				<< " instead of " << v_num_control_points[ir] << ") "
+				"for rotor #" << ir << " of " << num_rotors
+				<< " at line " << HP.GetLineData() << std::endl);
+			throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+		}
+	}
+
+	m_TrimTime.resize(5);
+	if (HP.IsKeyWord("trim" "times")) {
+		for (unsigned it = 0; it < 4; it++) {
+			m_TrimTime[it] = HP.GetReal();
+			if (it > 0 && m_TrimTime[it] <= m_TrimTime[it - 1]) {
+				silent_cerr("ModuleCHARM(" << uLabel << "): "
+					"TrimTime[" << it << "]=" << m_TrimTime[it] << " "
+					"incompatible with TrimTime[" << it - 1 << "]=" << m_TrimTime[it - 1] << " "
+					"at line " << HP.GetLineData() << std::endl);
+				throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+			}
+		}
+		m_TrimTime[4] = std::numeric_limits<doublereal>::max();
+
+	} else {
+		m_TrimTime[0] = std::numeric_limits<doublereal>::max();
+	}
+	m_TrimTimeIter = m_TrimTime.begin();
 
 	SetOutputFlag(pDM->fReadOutput(HP, Elem::LOADABLE));
 }
@@ -188,6 +450,123 @@ ModuleCHARM::bSectionalForces(void) const
 	return true;
 }
 
+void
+ModuleCHARM::Init_int(void)
+{
+	// this function must be called after all data has been set up
+	ASSERT(iFirstAssembly == 1);
+
+	initWPModule(1,		// FIXME: one aircraft only
+		&m_wpaircraft,
+		m_eval_pts.size(), m_eval_pts.size(), &m_eval_pts[0],
+		1,		// FIXME: areEvalPointsFixed?
+		&m_chglobal, &m_wpoptions);
+}
+
+void
+ModuleCHARM::Update_int(void)
+{
+	const Vec3& Xac(pCraft->GetXCurr());
+	// p, T not used yet
+	doublereal rho, c, p, T;
+	GetAirProps(Xac, rho, c, p, T);
+	const Vec3& Vac(pCraft->GetVCurr());
+	Mat3x3 Rac(pCraft->GetRCurr()*m_Rh);
+	Vec3 VBac(Rac.MulTV(Vac));
+	Vec3 WBac(Rac.MulTV(pCraft->GetWCurr()));
+	// FIXME: Euler angles? 123 or 321?  Rac or Rac^T?
+	Vec3 EBac = MatR2EulerAngles123(Rac);
+
+	// update global data
+	Vec3 Vinf(0.);
+	if (fGetAirVelocity(Vinf, Xac)) {
+		m_chglobal.inertial_winds[0] = Vinf(1);
+		m_chglobal.inertial_winds[1] = Vinf(2);
+		m_chglobal.inertial_winds[2] = Vinf(3);
+	}
+
+	doublereal dTime = m_Time.dGet();
+	m_chglobal.time_increment = dTime - m_chglobal.current_time;
+	m_chglobal.current_time = dTime;
+
+	// NOTE: ignore ground by now
+#if 0
+	m_chglobal.ground_position[0];
+	m_chglobal.ground_position[1];
+	m_chglobal.ground_position[2];
+	m_chglobal.ground_orientation[0];
+	m_chglobal.ground_orientation[1];
+	m_chglobal.ground_orientation[2];
+	m_chglobal.ground_center[0];
+	m_chglobal.ground_center[1];
+	m_chglobal.ground_center[2];
+#endif
+
+	// time-driven trim flag
+	while (dTime >= *m_TrimTimeIter) {
+		m_TrimTimeIter++;
+		m_chglobal.trim_flag++;
+		if (m_chglobal.trim_flag == 4) {
+			// FIXME: kupdgam may need to remain 1
+			m_wpoptions.kupdgam = 0;
+		}
+		silent_cout("ModuleCHARM(" << GetLabel() << "): "
+			"Time=" << dTime << " trim_flag=" << m_chglobal.trim_flag << std::endl);
+	}
+
+	// update aircraft data
+	m_wpaircraft.air_density = rho;
+	m_wpaircraft.inertial_position[0] = Xac(1);
+	m_wpaircraft.inertial_position[1] = Xac(2);
+	m_wpaircraft.inertial_position[2] = Xac(3);
+	m_wpaircraft.inertial_velocity[0] = Vac(1);
+	m_wpaircraft.inertial_velocity[1] = Vac(2);
+	m_wpaircraft.inertial_velocity[2] = Vac(3);
+	m_wpaircraft.body_velocity[0] = VBac(1);
+	m_wpaircraft.body_velocity[1] = VBac(2);
+	m_wpaircraft.body_velocity[2] = VBac(3);
+	m_wpaircraft.angular_rates[0] = WBac(1);
+	m_wpaircraft.angular_rates[1] = WBac(2);
+	m_wpaircraft.angular_rates[2] = WBac(3);
+	m_wpaircraft.euler_angles[0] = EBac(1);
+	m_wpaircraft.euler_angles[1] = EBac(2);
+	m_wpaircraft.euler_angles[2] = EBac(3);
+	// FIXME: R(i, j) or R(j, i)?
+	m_wpaircraft.T_inertial_to_body[0][0] = Rac(1, 1);
+	m_wpaircraft.T_inertial_to_body[0][1] = Rac(2, 1);
+	m_wpaircraft.T_inertial_to_body[0][2] = Rac(3, 1);
+	m_wpaircraft.T_inertial_to_body[1][0] = Rac(1, 2);
+	m_wpaircraft.T_inertial_to_body[1][1] = Rac(2, 2);
+	m_wpaircraft.T_inertial_to_body[1][2] = Rac(3, 2);
+	m_wpaircraft.T_inertial_to_body[2][0] = Rac(1, 3);
+	m_wpaircraft.T_inertial_to_body[2][1] = Rac(2, 3);
+	m_wpaircraft.T_inertial_to_body[2][2] = Rac(3, 3);
+
+	// TODO: rotor(s)
+
+	// actually call the module and update the induced velocity
+	// FIXME: the exact sequence needs to be carefully defined
+
+	// evaluate induced velocity at rotor blade evaluation points
+	// and off-rotor evaluation points
+	updateIndVelocity(&m_wpaircraft, &m_eval_pts[0]);
+
+	// update if needed
+	setCHARMGlobal(&m_chglobal);
+
+	// update if needed
+	setWPAircraft(0, &m_wpaircraft);
+
+	// update if needed
+	setCHARMOffRotor(m_eval_pts.size(), &m_eval_pts[0]);
+
+	// update wake
+	int isTrimming = 0;
+	chStatus status;
+	updateWake(isTrimming, &status);
+	// FIXME: test status?
+}
+
 Vec3
 ModuleCHARM::GetInducedVelocity(const Vec3& X) const
 {
@@ -196,7 +575,8 @@ ModuleCHARM::GetInducedVelocity(const Vec3& X) const
 
 void
 ModuleCHARM::AddSectionalForce(unsigned int uL,
-	const Vec3& F, const Vec3& M, doublereal dW, const Vec3& X)
+	const Vec3& F, const Vec3& M, doublereal dW,
+	const Vec3& X, const Mat3x3& R)
 {
 	std::cerr << "ModuleCHARM(" << GetLabel() << ")::AddSectionalForce: " << uL << std::endl;
 
@@ -216,7 +596,7 @@ ModuleCHARM::AddSectionalForce(unsigned int uL,
 		m_data[idx].X = Zero3;
 
 	} else {
-		const Mat3x3& R(pCraft->GetRCurr());
+		Mat3x3 R(pCraft->GetRCurr()*m_Rh);
 
 		// resolve force, moment and point in craft's reference frame
 		m_data_iter->F = R.MulTV(F);
@@ -226,6 +606,13 @@ ModuleCHARM::AddSectionalForce(unsigned int uL,
 
 		m_data_iter++;
 	}
+}
+
+void
+ModuleCHARM::AfterPredict(VectorHandler& X, VectorHandler& XP)
+{
+	// NO_OP;
+	Update_int();
 }
 
 void
@@ -285,7 +672,7 @@ ModuleCHARM::AssRes(SubVectorHandler& WorkVec,
 		iFirstAssembly--;
 	}
 
-	// re-initlize iterator to loop over point data
+	// re-initialize iterator to loop over point data
 	// when AddSectionalForces is called
 	m_data_iter = m_data.begin();
 
