bc03fb8f7a075ea6cdaf722c53bea1c62c2f302b
morandini <>
2011-01-26 16:58:08 +0000

    nlrheo_damper.c must be buildable without mbconfig.h

--- mbdyn/modules/module-damper/nlrheo_damper.c
+++ mbdyn/modules/module-damper/nlrheo_damper.c
@@ -28,7 +28,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include "mbconfig.h"           /* This goes first in every *.c,*.cc file */
 
 #include "nlrheo_damper.h"
 
