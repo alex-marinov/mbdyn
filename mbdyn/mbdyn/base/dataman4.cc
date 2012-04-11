710c3fdb57e8aadccad73d5e11f772b951bed218
masarati <>
2012-04-11 09:10:54 +0000

    clarify error message

--- mbdyn/mbdyn/base/dataman4.cc
+++ mbdyn/mbdyn/base/dataman4.cc
@@ -1496,7 +1496,7 @@ DataManager::ReadOneElem(MBDynParser& HP, unsigned int uLabel, const std::string
 			if (HP.IsKeyWord("rotor")) {
 				// continue
 			} else {
-				silent_cerr("InducedVelocity(" << uLabel << "): unknown \"induced velocity\" type at line " << HP.GetLineData() << " (missing \"rotor\"?)" << std::endl);
+				silent_cerr("InducedVelocity(" << uLabel << "): unknown \"induced velocity\" type at line " << HP.GetLineData() << " (missing \"rotor\" keyword?)" << std::endl);
 				throw DataManager::ErrGeneric(MBDYN_EXCEPT_ARGS);
 			}
 			break;
