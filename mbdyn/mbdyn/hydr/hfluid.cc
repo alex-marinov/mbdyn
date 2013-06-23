099d7f714a5da6e0814e14274a4541d24d265712
masarati <>
2013-06-23 18:41:57 +0000

    cleanup

--- mbdyn/mbdyn/hydr/hfluid.cc
+++ mbdyn/mbdyn/hydr/hfluid.cc
@@ -1,6 +1,6 @@
 /* $Header$ */
-/* 
- * MBDyn (C) is a multibody analysis code. 
+/*
+ * MBDyn (C) is a multibody analysis code.
  * http://www.mbdyn.org
  *
  * Copyright (C) 1996-2013
@@ -17,7 +17,7 @@
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation (version 2 of the License).
- * 
+ *
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -29,7 +29,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-/* 
+/*
  * Copyright 1999-2000 Lamberto Puggelli <puggelli@tiscalinet.it>
  * Dipartimento di Ingegneria Aerospaziale - Politecnico di Milano
  */
@@ -43,289 +43,328 @@
 #include "hfluid.h"
 #include "hfluid_.h"
 
-HydraulicFluid*
-ReadHydraulicFluid(MBDynParser& HP,
-		   unsigned int uLabel)
+/* HydraulicFluid - begin */
+
+HydraulicFluid::HydraulicFluid(unsigned int Label,
+	const doublereal dPres0,
+	const doublereal dTemp0)
+: WithLabel(Label), dPres0(dPres0), dTemp0(dTemp0)
 {
-   /* tipi di fluidi */
-   const char* sKeyWords[] = {
-      "uncompressible",		/* deprecated (typo) */
-      "incompressible",
-      "linear" "compressible",
-      "linear" "thermal" "compressible",
-      "super",
-      "exponential",
-      
-      NULL
-   };
-   
-   enum KeyWords {
-      UNKNOWN = -1,
-      
-      UNCOMPRESSIBLE = 0,	/* deprecated (typo) */
-      INCOMPRESSIBLE,
-      LINEARCOMPRESSIBLE,
-      LINEARTHERMALCOMPRESSIBLE,
-      SUPER,
-      EXPONENTIAL,
-      
-      LASTKEYWORD
-   };
-   
-   /* tabella delle parole chiave */
-   KeyTable K(HP, sKeyWords);
-   
-   /* lettura del tipo di drive */   
-   KeyWords CurrKeyWord;
-   if ((CurrKeyWord = KeyWords(HP.IsKeyWord())) == UNKNOWN) {
-      CurrKeyWord = INCOMPRESSIBLE;
-   }
-   
-   HydraulicFluid* pHF = NULL;
-   
-   switch (CurrKeyWord) {
-    case UNCOMPRESSIBLE:	/* deprecated (typo) */
-    case INCOMPRESSIBLE: {
-       
-       doublereal dDensity(0.);
-       if (HP.IsKeyWord("density")) {
-	  dDensity = HP.GetReal();
-       }
-       if (dDensity < std::numeric_limits<doublereal>::epsilon()) {
-	  silent_cerr("line " << HP.GetLineData()
-	    << ": illegal density " << dDensity
-	    << " for hydraulic fluid " << uLabel << std::endl);
-       }
-       
-       doublereal dViscosity(0.);
-       if (HP.IsKeyWord("viscosity")) {
-	  dViscosity = HP.GetReal();
-       }
-       if (dViscosity < std::numeric_limits<doublereal>::epsilon()) {
-	  silent_cerr("line " << HP.GetLineData() 
-	    << ": illegal viscosity " << dViscosity
-	    << std::endl);
-	  throw ErrGeneric(MBDYN_EXCEPT_ARGS);
-       }
-       
-       doublereal dPres0(-1.);
-       if (HP.IsKeyWord("pressure")) {
-	  dPres0 = HP.GetReal();
-	  if (dPres0 < 0.) {
-	     silent_cerr("line " << HP.GetLineData() 
-	       << ": illegal reference pressure " << dPres0 
-	       << " for hydraulic fluid " << uLabel << std::endl);
-	  }	       
-       }
-       
-       doublereal dTemp0(-1.);
-       if (HP.IsKeyWord("temperature")) {
-	  dTemp0 = HP.GetReal();
-	  if (dTemp0 < 0.) {
-	     silent_cerr("line " << HP.GetLineData()
-	       << ": illegal reference temperature " << dTemp0
-	       << " for hydraulic fluid " << uLabel << std::endl);
-	  }	       
-       }       
-       
-       SAFENEWWITHCONSTRUCTOR(pHF,
-			      IncompressibleHydraulicFluid,
-			      IncompressibleHydraulicFluid(uLabel, 
-							   dDensity, 
-							   dViscosity,
-							   dPres0,
-							   dTemp0));
-       break;
-    }
-      
-    case LINEARCOMPRESSIBLE: 
-    case SUPER:
-    case EXPONENTIAL: {
-       doublereal dDensity(0.);
-       doublereal dBeta(0.);
-       doublereal dPres0(0.);
-       if (HP.IsKeyWord("density")) {
-	  dDensity = HP.GetReal();
-	   
-	  if (HP.IsKeyWord("sound" "celerity")) {
-	     doublereal sound = HP.GetReal();
-	     if (sound < std::numeric_limits<doublereal>::epsilon()) {
-		silent_cerr("line " << HP.GetLineData()
-			<< ": illegal sound celerity " << sound
-		        << std::endl);
-		throw ErrGeneric(MBDYN_EXCEPT_ARGS);		
-	     } 
-	     dBeta = sound*sound*dDensity;
-	  } else { 
-	     dBeta = HP.GetReal();
-	     if (fabs(dBeta) < std::numeric_limits<doublereal>::epsilon()) {
-		silent_cerr("line " << HP.GetLineData()
-			  << ": illegal bulk modulus " << dBeta 
-		          << std::endl);
-		throw ErrGeneric(MBDYN_EXCEPT_ARGS);
-	     }
-	  }
-	   
-	  if (dDensity < std::numeric_limits<doublereal>::epsilon()) {
-	     silent_cerr("line " << HP.GetLineData()
-	       << ": illegal density " << dDensity
-	       << " for hydraulic fluid " << uLabel << std::endl);
-	  }
-	  
-	  dPres0 = HP.GetReal();
-	  if (dPres0 < 0.) {
-	     silent_cerr("line " << HP.GetLineData() 
-	       << ": illegal reference pressure " << dPres0 
-	       << " for hydraulic fluid " << uLabel << std::endl);
-	  }	       
-       }
-       
-       doublereal dViscosity(0.);
-       if (HP.IsKeyWord("viscosity")) {
-	  dViscosity = HP.GetReal();
-       }
-        
-       if (dViscosity < std::numeric_limits<doublereal>::epsilon()) {
-	  silent_cerr("line " << HP.GetLineData() 
-	    << ": illegal viscosity " << dViscosity
-	    << std::endl);
-	  throw ErrGeneric(MBDYN_EXCEPT_ARGS);
-       }
-       
-       doublereal dTemp0(-1.);
-       if (HP.IsKeyWord("temperature")) {
-	  dTemp0 = HP.GetReal();
-	  if (dTemp0 < 0.) {
-	     silent_cerr("line " << HP.GetLineData()
-	       << ": illegal reference temperature " << dTemp0
-	       << " for hydraulic fluid " << uLabel << std::endl);
-	  }	       
-       }
-       
-       switch (CurrKeyWord) {
-	default:
-	  throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+	NO_OP;
+}
 
-	case LINEARCOMPRESSIBLE: 
-	  SAFENEWWITHCONSTRUCTOR(pHF,
-				 LinearCompressibleHydraulicFluid,
-				 LinearCompressibleHydraulicFluid(uLabel, 
-								  dDensity, 
-								  dBeta,
-								  dPres0,
-								  dViscosity,
-								  dTemp0));
-	  break;
+HydraulicFluid::HydraulicFluid(const HydraulicFluid& HF)
+: WithLabel(HF.GetLabel()), dPres0(HF.dPres0), dTemp0(HF.dTemp0)
+{
+	NO_OP;
+}
 
+HydraulicFluid::~HydraulicFluid(void)
+{
+      NO_OP;
+}
+
+doublereal
+HydraulicFluid::dGetPres0(void) const
+{
+	ASSERT(dPres0 != -1.);
+	return dPres0;
+}
+
+doublereal
+HydraulicFluid::dGetTemp0(void) const
+{
+	ASSERT(dTemp0 != -1.);
+	return dTemp0;
+}
+
+/* HydraulicFluid - end */
+
+
+HydraulicFluid *
+ReadHydraulicFluid(MBDynParser& HP, unsigned int uLabel)
+{
+	/* tipi di fluidi */
+	const char* sKeyWords[] = {
+		"uncompressible",		/* deprecated (typo) */
+		"incompressible",
+		"linear" "compressible",
+		"linear" "thermal" "compressible",
+		"super",
+		"exponential",
+
+		NULL
+	};
+
+	enum KeyWords {
+		UNKNOWN = -1,
+
+		UNCOMPRESSIBLE = 0,	/* deprecated (typo) */
+		INCOMPRESSIBLE,
+		LINEARCOMPRESSIBLE,
+		LINEARTHERMALCOMPRESSIBLE,
+		SUPER,
+		EXPONENTIAL,
+
+		LASTKEYWORD
+	};
+
+	/* tabella delle parole chiave */
+	KeyTable K(HP, sKeyWords);
+
+	/* lettura del tipo di drive */
+	KeyWords CurrKeyWord = KeyWords(HP.IsKeyWord());
+	if (CurrKeyWord == UNKNOWN) {
+		CurrKeyWord = INCOMPRESSIBLE;
+	}
+
+	HydraulicFluid* pHF = 0;
+
+	switch (CurrKeyWord) {
+	case UNCOMPRESSIBLE:	/* deprecated (typo) */
+	case INCOMPRESSIBLE: {
+		doublereal dDensity(0.);
+		if (HP.IsKeyWord("density")) {
+			dDensity = HP.GetReal();
+		}
+
+		if (dDensity < std::numeric_limits<doublereal>::epsilon()) {
+			silent_cerr("line " << HP.GetLineData()
+				<< ": illegal density " << dDensity
+				<< " for hydraulic fluid " << uLabel << std::endl);
+	  		throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+		}
+
+		doublereal dViscosity(0.);
+		if (HP.IsKeyWord("viscosity")) {
+			dViscosity = HP.GetReal();
+		}
+
+		if (dViscosity < std::numeric_limits<doublereal>::epsilon()) {
+			silent_cerr("line " << HP.GetLineData()
+				<< ": illegal viscosity " << dViscosity
+				<< std::endl);
+			throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+		}
+
+		doublereal dPres0(-1.);
+		if (HP.IsKeyWord("pressure")) {
+			dPres0 = HP.GetReal();
+			if (dPres0 < 0.) {
+				silent_cerr("line " << HP.GetLineData()
+					<< ": illegal reference pressure " << dPres0
+					<< " for hydraulic fluid " << uLabel << std::endl);
+			}
+		}
+
+		doublereal dTemp0(-1.);
+		if (HP.IsKeyWord("temperature")) {
+			dTemp0 = HP.GetReal();
+			if (dTemp0 < 0.) {
+				silent_cerr("line " << HP.GetLineData()
+					<< ": illegal reference temperature " << dTemp0
+					<< " for hydraulic fluid " << uLabel << std::endl);
+			}
+		}
+
+		SAFENEWWITHCONSTRUCTOR(pHF,
+			IncompressibleHydraulicFluid,
+			IncompressibleHydraulicFluid(uLabel,
+				dDensity,
+				dViscosity,
+				dPres0,
+				dTemp0));
+		} break;
+
+	case LINEARCOMPRESSIBLE:
 	case SUPER:
-	  SAFENEWWITHCONSTRUCTOR(pHF,
-				 SuperHydraulicFluid,
-				 SuperHydraulicFluid(uLabel, 
-						     dDensity, 
-						     dBeta,
-						     dPres0,
-						     dViscosity,
-						     dTemp0));
-	  break;
+	case EXPONENTIAL: {
+		doublereal dDensity(0.);
+		doublereal dBeta(0.);
+		doublereal dPres0(0.);
+		if (HP.IsKeyWord("density")) {
+			dDensity = HP.GetReal();
 
-	case EXPONENTIAL:
-	  doublereal dPsat = HP.GetReal();
-	  
-	  SAFENEWWITHCONSTRUCTOR(pHF,
-				 ExpHydraulicFluid,
-				 ExpHydraulicFluid(uLabel, 
-						      dDensity, 
-						      dBeta,
-						      dPres0,
-						      dPsat,
-						      dViscosity,
-						      dTemp0));
-	  break;
-       }
-       
-       break;
-    }
+			if (HP.IsKeyWord("sound" "celerity")) {
+				doublereal sound = HP.GetReal();
+				if (sound < std::numeric_limits<doublereal>::epsilon()) {
+					silent_cerr("line " << HP.GetLineData()
+						<< ": illegal sound celerity " << sound
+						<< std::endl);
+					throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+				}
 
-    case LINEARTHERMALCOMPRESSIBLE: {
-       doublereal dDensity(0.);
-       doublereal dBeta(0.);
-       doublereal dPres0(0.);
-       doublereal dAlpha(0.);
-       doublereal dTemp0(0.);
-       if (HP.IsKeyWord("density")) {
-	  dDensity = HP.GetReal();
-	 
-	  if (HP.IsKeyWord("sound" "celerity")) {
-	     doublereal sound = HP.GetReal();
-	     if (sound < std::numeric_limits<doublereal>::epsilon()) {
+				dBeta = sound*sound*dDensity;
+			} else {
+				dBeta = HP.GetReal();
+				if (std::abs(dBeta) < std::numeric_limits<doublereal>::epsilon()) {
+					silent_cerr("line " << HP.GetLineData()
+						<< ": illegal bulk modulus " << dBeta
+						<< std::endl);
+					throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+				}
+			}
+
+			if (dDensity < std::numeric_limits<doublereal>::epsilon()) {
+				silent_cerr("line " << HP.GetLineData()
+					<< ": illegal density " << dDensity
+					<< " for hydraulic fluid " << uLabel << std::endl);
+			}
+
+			dPres0 = HP.GetReal();
+			if (dPres0 < 0.) {
+				silent_cerr("line " << HP.GetLineData()
+					<< ": illegal reference pressure " << dPres0
+					<< " for hydraulic fluid " << uLabel << std::endl);
+			}
+		}
+
+		doublereal dViscosity(0.);
+		if (HP.IsKeyWord("viscosity")) {
+			dViscosity = HP.GetReal();
+		}
+
+		if (dViscosity < std::numeric_limits<doublereal>::epsilon()) {
+			silent_cerr("line " << HP.GetLineData()
+				<< ": illegal viscosity " << dViscosity
+				<< std::endl);
+			throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+		}
+
+		doublereal dTemp0(-1.);
+		if (HP.IsKeyWord("temperature")) {
+			dTemp0 = HP.GetReal();
+			if (dTemp0 < 0.) {
+				silent_cerr("line " << HP.GetLineData()
+					<< ": illegal reference temperature " << dTemp0
+					<< " for hydraulic fluid " << uLabel << std::endl);
+			}
+		}
+
+		switch (CurrKeyWord) {
+		default:
+			throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+
+		case LINEARCOMPRESSIBLE:
+			SAFENEWWITHCONSTRUCTOR(pHF,
+				LinearCompressibleHydraulicFluid,
+				LinearCompressibleHydraulicFluid(uLabel,
+					dDensity,
+					dBeta,
+					dPres0,
+					dViscosity,
+					dTemp0));
+			break;
+
+		case SUPER:
+			SAFENEWWITHCONSTRUCTOR(pHF,
+				SuperHydraulicFluid,
+				SuperHydraulicFluid(uLabel,
+					dDensity,
+					dBeta,
+					dPres0,
+					dViscosity,
+					dTemp0));
+			break;
+
+		case EXPONENTIAL:
+			doublereal dPsat = HP.GetReal();
+
+			SAFENEWWITHCONSTRUCTOR(pHF,
+				ExpHydraulicFluid,
+				ExpHydraulicFluid(uLabel,
+					dDensity,
+					dBeta,
+					dPres0,
+					dPsat,
+					dViscosity,
+					dTemp0));
+			break;
+		}
+
+		} break;
+
+	case LINEARTHERMALCOMPRESSIBLE: {
+		doublereal dDensity(0.);
+		doublereal dBeta(0.);
+		doublereal dPres0(0.);
+		doublereal dAlpha(0.);
+		doublereal dTemp0(0.);
+		if (HP.IsKeyWord("density")) {
+			dDensity = HP.GetReal();
+
+			if (HP.IsKeyWord("sound" "celerity")) {
+				doublereal sound = HP.GetReal();
+				if (sound < std::numeric_limits<doublereal>::epsilon()) {
+					silent_cerr("line " << HP.GetLineData()
+						<< ": illegal sound celerity " << sound
+						<< " for hydraulic fluid " << uLabel << std::endl);
+					throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+				}
+
+				dBeta = sound*sound*dDensity;
+
+			} else {
+				dBeta = HP.GetReal();
+				if (std::abs(dBeta) < std::numeric_limits<doublereal>::epsilon()) {
+					silent_cerr("line " << HP.GetLineData()
+						<< ": illegal bulk modulus " << dBeta
+						<< " for hydraulic fluid " << uLabel << std::endl);
+					throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+				}
+			}
+
+			if (dDensity < std::numeric_limits<doublereal>::epsilon()) {
+				silent_cerr("line " << HP.GetLineData()
+					<< ": illegal density " << dDensity
+					<< " for hydraulic fluid " << uLabel << std::endl);
+			}
+
+			dPres0 = HP.GetReal();
+			if (dPres0 < 0.) {
+				silent_cerr("line " << HP.GetLineData()
+					<< ": illegal reference pressure " << dPres0
+					<< " for hydraulic fluid " << uLabel << std::endl);
+			}
+
+			dAlpha = HP.GetReal();
+
+			dTemp0 = HP.GetReal();
+			if (dTemp0 < 0.) {
+				silent_cerr("line " << HP.GetLineData()
+					<< ": illegal reference temperature " << dTemp0
+					<< " for hydraulic fluid " << uLabel << std::endl);
+			}
+		}
+
+		doublereal dViscosity(0.);
+		if (HP.IsKeyWord("viscosity")) {
+			dViscosity = HP.GetReal();
+		}
+
+		if (dViscosity < std::numeric_limits<doublereal>::epsilon()) {
+			silent_cerr("line " << HP.GetLineData()
+				<< ": illegal viscosity " << dViscosity
+				<< std::endl);
+			throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+		}
+
+		SAFENEWWITHCONSTRUCTOR(pHF,
+			LinearCompressibleTHydraulicFluid,
+			LinearCompressibleTHydraulicFluid(uLabel,
+				dDensity,
+				dBeta,
+				dPres0,
+				dAlpha,
+				dTemp0,
+				dViscosity));
+		} break;
+
+	default:
 		silent_cerr("line " << HP.GetLineData()
-		  << ": illegal sound celerity " << sound
-	          << " for hydraulic fluid " << uLabel << std::endl);
-		throw ErrGeneric(MBDYN_EXCEPT_ARGS);		
-	     } 
-	     dBeta = sound*sound*dDensity;
-	  } else {
-	     dBeta = HP.GetReal();
-	     if (fabs(dBeta) < std::numeric_limits<doublereal>::epsilon()) {
-		silent_cerr("line " << HP.GetLineData()
-		  << ": illegal bulk modulus " << dBeta
-	          << " for hydraulic fluid " << uLabel << std::endl);
+			<< ": unknown hydraulic fluid type" << std::endl);
 		throw ErrGeneric(MBDYN_EXCEPT_ARGS);
-	     }
-	  }
-	  
-	  if (dDensity < std::numeric_limits<doublereal>::epsilon()) {
-	     silent_cerr("line " << HP.GetLineData()
-	       << ": illegal density " << dDensity
-	       << " for hydraulic fluid " << uLabel << std::endl);
-	  }
-	  
-	  dPres0 = HP.GetReal();
-	  if (dPres0 < 0.) {
-	     silent_cerr("line " << HP.GetLineData() 
-	       << ": illegal reference pressure " << dPres0 
-	       << " for hydraulic fluid " << uLabel << std::endl);
-	  }	       
+	}
 
-	  dAlpha = HP.GetReal();
-	  
-	  dTemp0 = HP.GetReal();
-	  if (dTemp0 < 0.) {
-	     silent_cerr("line " << HP.GetLineData()
-	       << ": illegal reference temperature " << dTemp0
-	       << " for hydraulic fluid " << uLabel << std::endl);
-	  }	       
-       }
-       
-       doublereal dViscosity(0.);
-       if (HP.IsKeyWord("viscosity")) {
-	  dViscosity = HP.GetReal();
-       }
-       
-       if (dViscosity < std::numeric_limits<doublereal>::epsilon()) {
-	  silent_cerr("line " << HP.GetLineData() 
-	    << ": illegal viscosity " << dViscosity
-	    << std::endl);
-	  throw ErrGeneric(MBDYN_EXCEPT_ARGS);
-       }
-       
-       SAFENEWWITHCONSTRUCTOR(pHF,
-			      LinearCompressibleTHydraulicFluid,
-			      LinearCompressibleTHydraulicFluid(uLabel,
-								dDensity,
-								dBeta,
-								dPres0,
-								dAlpha,
-								dTemp0,
-								dViscosity));
-       break;
-    }
-
-    default:
-      silent_cerr("line " << HP.GetLineData()
-		      << ": unknown hydraulic fluid type" << std::endl);
-      throw ErrGeneric(MBDYN_EXCEPT_ARGS);
-   }
-   
-   return pHF;
+	return pHF;
 }
--- mbdyn/mbdyn/hydr/hfluid.h
+++ mbdyn/mbdyn/hydr/hfluid.h
@@ -1,6 +1,6 @@
 /* $Header$ */
-/* 
- * MBDyn (C) is a multibody analysis code. 
+/*
+ * MBDyn (C) is a multibody analysis code.
  * http://www.mbdyn.org
  *
  * Copyright (C) 1996-2013
@@ -17,7 +17,7 @@
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation (version 2 of the License).
- * 
+ *
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -29,7 +29,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-/* 
+/*
  * Copyright 1999-2000 Lamberto Puggelli <puggelli@tiscalinet.it>
  * Dipartimento di Ingegneria Aerospaziale - Politecnico di Milano
  */
@@ -44,72 +44,60 @@
 /* HydraulicFluid - begin */
 
 class HydraulicFluid : public WithLabel {
- protected: 
-   const doublereal dPres0;
-   const doublereal dTemp0;
-   
- public:
-   HydraulicFluid(unsigned int Label,
-		  const doublereal dPres0 = -1.,
-		  const doublereal dTemp0 = -1.) 
-     : WithLabel(Label), dPres0(dPres0), dTemp0(dTemp0) {
-	NO_OP;
-     };
-   HydraulicFluid(const HydraulicFluid& HF)
-     : WithLabel(HF.GetLabel()), dPres0(HF.dPres0), dTemp0(HF.dTemp0) {
-	NO_OP;
-     };
-   
-   virtual ~HydraulicFluid() {
-      NO_OP;
-   };
-   
-   /* crea una copia dell'HF */
-   virtual HydraulicFluid* pCopy(void) const = 0;
+protected:
+	const doublereal dPres0;
+	const doublereal dTemp0;
 
-   /* funzioni relative alla densita'; possono essere usate senza argomenti
-    * (fluido incomprimibile), con un argomento, o con due (effetto di T) */
-   virtual doublereal dGetDensity(void) const = 0;
-   virtual doublereal dGetDensity(const doublereal& dPres) const = 0;
-   virtual doublereal dGetDensity(const doublereal& dPres,
-				  const doublereal& dTemp) const = 0;
-   virtual doublereal dGetDensityDPres(void) const = 0;
-   virtual doublereal dGetDensityDPres(const doublereal& dPres) const = 0;
-   virtual doublereal dGetDensityDPres(const doublereal& dPres,
-				       const doublereal& dTemp) const = 0;
-   virtual doublereal dGetDensityDTemp(void) const = 0;
-   virtual doublereal dGetDensityDTemp(const doublereal& dPres) const = 0;
-   virtual doublereal dGetDensityDTemp(const doublereal& dPres,
-				       const doublereal& dTemp) const = 0;
-   virtual doublereal dGetViscosity(void) const = 0;
-   virtual doublereal dGetViscosity(const doublereal& dPres) const = 0;
-   virtual doublereal dGetViscosity(const doublereal& dPres,
-				    const doublereal& dTemp) const = 0;
-   enum Re { UPPER, LOWER };
-   virtual inline doublereal dGetRe(Re which);
+public:
+	HydraulicFluid(unsigned int Label,
+		const doublereal dPres0 = -1.,
+		const doublereal dTemp0 = -1.);
+	HydraulicFluid(const HydraulicFluid& HF);
+
+	virtual ~HydraulicFluid();
+
+	/* crea una copia dell'HF */
+	virtual HydraulicFluid* pCopy(void) const = 0;
+
+	/* funzioni relative alla densita'; possono essere usate senza argomenti
+	 * (fluido incomprimibile), con un argomento, o con due (effetto di T) */
+	virtual doublereal dGetDensity(void) const = 0;
+	virtual doublereal dGetDensity(const doublereal& dPres) const = 0;
+	virtual doublereal dGetDensity(const doublereal& dPres,
+		const doublereal& dTemp) const = 0;
+	virtual doublereal dGetDensityDPres(void) const = 0;
+	virtual doublereal dGetDensityDPres(const doublereal& dPres) const = 0;
+	virtual doublereal dGetDensityDPres(const doublereal& dPres,
+		const doublereal& dTemp) const = 0;
+	virtual doublereal dGetDensityDTemp(void) const = 0;
+	virtual doublereal dGetDensityDTemp(const doublereal& dPres) const = 0;
+	virtual doublereal dGetDensityDTemp(const doublereal& dPres,
+		const doublereal& dTemp) const = 0;
+	virtual doublereal dGetViscosity(void) const = 0;
+	virtual doublereal dGetViscosity(const doublereal& dPres) const = 0;
+	virtual doublereal dGetViscosity(const doublereal& dPres,
+		const doublereal& dTemp) const = 0;
+
+	enum Re { UPPER, LOWER };
+	virtual inline doublereal dGetRe(Re which);
+
 #if 0
-   virtual doublereal dGetRe(Re which, const doublereal& dPres) = 0;
-   virtual doublereal dGetRe(Re which, const doublereal& dPres,
-		   const doublereal& dTemp) = 0;
+	virtual doublereal dGetRe(Re which, const doublereal& dPres) = 0;
+	virtual doublereal dGetRe(Re which, const doublereal& dPres,
+		const doublereal& dTemp) = 0;
 #endif
-   
-   virtual doublereal dGetPres0(void) const {
-      ASSERT(dPres0 != -1.);
-      return dPres0;
-   };
-   virtual doublereal dGetTemp0(void) const {
-      ASSERT(dTemp0 != -1.);
-      return dTemp0;
-   };
+
+	virtual doublereal dGetPres0(void) const;
+	virtual doublereal dGetTemp0(void) const;
 };
 
-inline doublereal 
+inline doublereal
 HydraulicFluid::dGetRe(HydraulicFluid::Re which)
 {
 	switch (which) {
 	case HydraulicFluid::LOWER:
 		return 2000.;
-		
+
 	case HydraulicFluid::UPPER:
 		return 4000.;
 
@@ -118,10 +106,10 @@ HydraulicFluid::dGetRe(HydraulicFluid::Re which)
 	}
 }
 
-/* legge un fluido idraulico; non va usata direttamente, viene invece 
+/* legge un fluido idraulico; non va usata direttamente, viene invece
  * gestita dal parser specializzato di MBDyn, MBDynParser (mbpar.h) */
 class MBDynParser;
-extern HydraulicFluid* ReadHydraulicFluid(MBDynParser& HP, unsigned int uLabel);
+extern HydraulicFluid * ReadHydraulicFluid(MBDynParser& HP, unsigned int uLabel);
 
 /* HydraulicFluid - end */
 
