4c30df75e45446181d42f2f094f362b630c50b16
masarati <>
2013-07-23 21:16:24 +0000

    import latest from Eduardo

--- mbdyn/modules/module-fabricate/module-fabricate.cc
+++ mbdyn/modules/module-fabricate/module-fabricate.cc
@@ -1,4 +1,3 @@
-/* $Header$ */
 /*
  * MBDyn (C) is a multibody analysis code.
  * http://www.mbdyn.org
@@ -27,11 +26,10 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-/*
- * Author: Eduardo Okabe, 2013
- */
 
-// Simple Gear Joint v2 - July 2nd, 2013
+// Library of components of "digital fabrication" machines (alpha version) 
+//
+// Gear Joint v2c - July 11th, 2013
 
 #include "mbconfig.h"           /* This goes first in every *.c,*.cc file */
 
@@ -42,7 +40,7 @@
 #include "userelem.h"
 #include "Rot.hh"
 
-class SimpleGearJoint
+class GearJoint
 : virtual public Elem, public UserDefinedElem {
 private:
    const StructNode* pNode1;
@@ -50,18 +48,21 @@ private:
    const StructNode* pNodeRef;
    doublereal Gear_r1;
    doublereal Gear_r2;
+   doublereal MTmp;
    Vec3 ThetaOut1;
    Vec3 ThetaOut2;
    Vec3 M1;
    Vec3 M2;
    Vec3 kvector;
-   Mat3x3 R1tilde;
-   Mat3x3 R2tilde;
+   Mat3x3 R1rtilde;
+   Mat3x3 R2rtilde;
+   Mat3x3 R1c;
+   Mat3x3 R2c;
    
 public:
-	SimpleGearJoint(unsigned uLabel, const DofOwner *pDO,
+	GearJoint(unsigned uLabel, const DofOwner *pDO,
 		DataManager* pDM, MBDynParser& HP);
-	virtual ~SimpleGearJoint(void);
+	virtual ~GearJoint(void);
 
 	virtual void Output(OutputHandler& OH) const;
 	virtual void WorkSpaceDim(integer* piNumRows, integer* piNumCols) const;
@@ -94,7 +95,7 @@ public:
 	void AfterConvergence(const VectorHandler& X, const VectorHandler& XP);
 };
 
-SimpleGearJoint::SimpleGearJoint(
+GearJoint::GearJoint(
 	unsigned uLabel, const DofOwner *pDO,
 	DataManager* pDM, MBDynParser& HP)
 : Elem(uLabel, flag(0)),
@@ -105,7 +106,7 @@ pNode1(0), pNode2(0), pNodeRef(0), Gear_r1(0.), Gear_r2(0.)
 	if (HP.IsKeyWord("help")) {
 		silent_cout(
 "									\n"
-"Module: 	Simple Gear Joint						\n"
+"Module: 	Gear Joint						\n"
 			<< std::endl);
 
 		if (!HP.IsArg()) {
@@ -122,84 +123,122 @@ pNode1(0), pNode2(0), pNodeRef(0), Gear_r1(0.), Gear_r2(0.)
    pNode1 = pDM->ReadNode<const StructNode, Node::STRUCTURAL>(HP);
 
    if (!pNode1) {
-          silent_cerr("Simple Gear Joint (" << GetLabel() << ") - node 1: structural node expected at line " << HP.GetLineData() << std::endl);
+          silent_cerr("Gear Joint (" << GetLabel() << ") - node 1: structural node expected at line " << HP.GetLineData() << std::endl);
           throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+   }
+
+   ReferenceFrame RF1(pNode1);
+   
+   R1rtilde = Mat3x3(Eye3);
+
+	// Read the relative reference frame of gear 1, if supplied:
+   if (HP.IsKeyWord("orientation")) {
+        DEBUGCOUT("Rotation orientation matrix of node 1 is supplied" << std::endl);
+        R1rtilde = HP.GetRotRel(RF1);
    }
 
 	// Read the node of gear 2 from .mbd file:
    pNode2 = pDM->ReadNode<const StructNode, Node::STRUCTURAL>(HP);
 
    if (!pNode2) {
-          silent_cerr("Simple Gear Joint (" << GetLabel() << ") - node 2: structural node expected at line " << HP.GetLineData() << std::endl);
-          throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+      silent_cerr("Gear Joint (" << GetLabel() << ") - node 2: structural node expected at line " << HP.GetLineData() << std::endl);
+      throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+   }
+
+   ReferenceFrame RF2(pNode2);
+   
+   R2rtilde = Mat3x3(Eye3);
+
+	// Read the relative reference frame of gear 2, if supplied:
+   if (HP.IsKeyWord("orientation")) {
+        DEBUGCOUT("Rotation orientation matrix of node 1 is supplied" << std::endl);
+        R2rtilde = HP.GetRotRel(RF2);
    }
 
 	// Read the reference node (support structure) from .mbd file:
    pNodeRef = pDM->ReadNode<const StructNode, Node::STRUCTURAL>(HP);
 
    if (!pNodeRef) {
-          silent_cerr("Simple Gear Joint (" << GetLabel() << ") - reference node: structural node expected at line " << HP.GetLineData() << std::endl);
-          throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+      silent_cerr("Gear Joint (" << GetLabel() << ") - reference node: structural node expected at line " << HP.GetLineData() << std::endl);
+      throw ErrGeneric(MBDYN_EXCEPT_ARGS);
    }
 
-	// Read the radius of gear 1 from .mbd file:
-   Gear_r1 = HP.GetReal();
 
-	// Read the radius of gear 2 from .mbd file:
-   Gear_r2 = HP.GetReal();
+   if (HP.IsKeyWord("ratio")) {
+	   // Set radius of gear 1 equal to 1.:
+      Gear_r1 = 1.;
+	   // Read the ratio (r2/r1) from .mbd file:
+      Gear_r2 = HP.GetReal();
+   }
+   else {
+	   // Read the radius of gear 1 from .mbd file:
+      Gear_r1 = HP.GetReal();
+	   // Read the radius of gear 2 from .mbd file:
+      Gear_r2 = HP.GetReal();   
+   }   
+
 
 	// Activate element output:
 	SetOutputFlag(pDM->fReadOutput(HP, Elem::LOADABLE));
 
    kvector = Vec3(0., 0., 1.);
   
-   Mat3x3 R1(pNode1->GetRCurr());
-   Mat3x3 R2(pNode2->GetRCurr());
+   Mat3x3 R1(pNode1->GetRCurr()*R1rtilde);
+   Mat3x3 R2(pNode2->GetRCurr()*R2rtilde);
    Mat3x3 RRf(pNodeRef->GetRCurr());     
+
+	// Disable initial orientation correction (correction is enabled by default):
+   if (HP.IsKeyWord("disable" "initial" "correction")) {
+      R1c = Mat3x3(Eye3);  
+      R2c = Mat3x3(Eye3);  
+   }
+   else {
+      R1c = RRf.MulTM(R1);  
+      R2c = RRf.MulTM(R2);  
+   }   
   
-   R1tilde = RRf.MulTM(R1);  
-   R2tilde = RRf.MulTM(R2);  
-  
-   ThetaOut1 = RotManip::VecRot(R1.MulTM(RRf)*R1tilde);
-   ThetaOut2 = RotManip::VecRot(R2.MulTM(RRf)*R2tilde);
+	// If correction is enabled, both angles should be zero:  
+   ThetaOut1 = RotManip::VecRot(R1.MulTM(RRf)*R1c);
+   ThetaOut2 = RotManip::VecRot(R2.MulTM(RRf)*R2c);
 
 }
 
-SimpleGearJoint::~SimpleGearJoint(void)
+GearJoint::~GearJoint(void)
 {
 	// destroy private data
 	NO_OP;
 }
 
 void
-SimpleGearJoint::Output(OutputHandler& OH) const
+GearJoint::Output(OutputHandler& OH) const
 {
+
    if (fToBeOutput()) {
    std::ostream& out = OH.Loadable();
    out << std::setw(8) << GetLabel()
-      << " " << M1
-      << " " << M2
-      << " " << -M1-M2
-      << " " << ThetaOut1
-      << " " << ThetaOut2
+      << " " << M1         // Moment on node 1
+      << " " << M2         // Moment on node 2
+      << " " << -M1-M2     // Moment on reference node
+      << " " << ThetaOut1  // Actual relative rotation of node 1
+      << " " << ThetaOut2  // Actual relative rotation of node 2
       << std::endl;
    }
 }
 
 void
-SimpleGearJoint::WorkSpaceDim(integer* piNumRows, integer* piNumCols) const
+GearJoint::WorkSpaceDim(integer* piNumRows, integer* piNumCols) const
 {
 	*piNumRows = 10;
 	*piNumCols = 10;
 }
 
 VariableSubMatrixHandler& 
-SimpleGearJoint::AssJac(VariableSubMatrixHandler& WorkMat,
+GearJoint::AssJac(VariableSubMatrixHandler& WorkMat,
 	doublereal dCoef, 
 	const VectorHandler& XCurr,
 	const VectorHandler& XPrimeCurr)
 {
-   DEBUGCOUT("Entering SimpleGearJoint::AssJac()" << std::endl);
+   DEBUGCOUT("Entering GearJoint::AssJac()" << std::endl);
 
    FullSubMatrixHandler& WM = WorkMat.SetFull();
    /* Change the dimension of the submatrix based on the constraint demand */
@@ -233,28 +272,27 @@ SimpleGearJoint::AssJac(VariableSubMatrixHandler& WorkMat,
    WM.PutRowIndex(10, iFirstReactionIndex);
    WM.PutColIndex(10, iFirstReactionIndex);
 
-   Mat3x3 R1(pNode1->GetRCurr());
-   Mat3x3 R2(pNode2->GetRCurr());
-   Mat3x3 RRf(pNodeRef->GetRCurr());
-   
-   Vec3 theta1(RotManip::VecRot(R1.MulTM(RRf)*R1tilde));
-   Vec3 theta2(RotManip::VecRot(R2.MulTM(RRf)*R2tilde));
-
-   //unwrapping function:
-   theta1 = Unwrap(ThetaOut1, theta1);
-   theta2 = Unwrap(ThetaOut2, theta2);
+   // Retrieve node rotation matrices:
+   Mat3x3 R1r(pNode1->GetRCurr()*R1rtilde);
+   Mat3x3 R2r(pNode2->GetRCurr()*R2rtilde);
+   Mat3x3 RRf(pNodeRef->GetRCurr());     
+  
+   // Calculate relative angles:
+   Vec3 theta1(RotManip::VecRot(R1r.MulTM(RRf)*R1c));
+   Vec3 theta2(RotManip::VecRot(R2r.MulTM(RRf)*R2c));
    
    Mat3x3 Gamma1IT(RotManip::DRot_IT(theta1));
    Mat3x3 Gamma2IT(RotManip::DRot_IT(theta2));
    
-   Mat3x3 MTmp1 = (-Mat3x3(MatCross, R1*Gamma1IT*kvector) + R1*Gamma1IT*RotManip::Elle(-theta1, Gamma1IT*kvector))*Gear_r1*dCoef;
-   Mat3x3 MTmp2 = (-Mat3x3(MatCross, R2*Gamma2IT*kvector) + R2*Gamma2IT*RotManip::Elle(-theta2, Gamma2IT*kvector))*Gear_r2*dCoef;
+   // MTmp = XCurr(iFirstReactionIndex); // Its value comes from residue calculation
+   Mat3x3 MTmp1 = (-Mat3x3(MatCross, R1r*Gamma1IT*kvector) + R1r*Gamma1IT*RotManip::Elle(-theta1, Gamma1IT*kvector))*MTmp*Gear_r1*dCoef;
+   Mat3x3 MTmp2 = (-Mat3x3(MatCross, R2r*Gamma2IT*kvector) + R2r*Gamma2IT*RotManip::Elle(-theta2, Gamma2IT*kvector))*MTmp*Gear_r2*dCoef;
 
-   Vec3 C1(R1*Gamma1IT*kvector*Gear_r1);   
-   Vec3 C2(R2*Gamma2IT*kvector*Gear_r2);
+   Vec3 C1(R1r*Gamma1IT*kvector*Gear_r1);   
+   Vec3 C2(R2r*Gamma2IT*kvector*Gear_r2);
 
-   Vec3 dTmp1 = kvector*RotManip::DRot_I(theta1).MulMT(R1)*Gear_r1*dCoef;
-   Vec3 dTmp2 = kvector*RotManip::DRot_I(theta2).MulMT(R2)*Gear_r2*dCoef;
+   Vec3 dTmp1 = kvector*RotManip::DRot_I(theta1).MulMT(R1r)*Gear_r1*dCoef;
+   Vec3 dTmp2 = kvector*RotManip::DRot_I(theta2).MulMT(R2r)*Gear_r2*dCoef;
    
    // Perturbation of couple in node 1:
    WM.Add(1, 1, MTmp1);
@@ -279,12 +317,12 @@ SimpleGearJoint::AssJac(VariableSubMatrixHandler& WorkMat,
 }
 
 SubVectorHandler& 
-SimpleGearJoint::AssRes(SubVectorHandler& WorkVec,
+GearJoint::AssRes(SubVectorHandler& WorkVec,
 	doublereal dCoef,
 	const VectorHandler& XCurr, 
 	const VectorHandler& XPrimeCurr)
 {
-   DEBUGCOUT("Entering SimpleGearJoint::AssRes()" << std::endl);
+   DEBUGCOUT("Entering GearJoint::AssRes()" << std::endl);
 
    /* Change the dimension of the vector based on the constraint demand */
    integer iNumRows = 0;
@@ -306,40 +344,37 @@ SimpleGearJoint::AssRes(SubVectorHandler& WorkVec,
 
    WorkVec.PutRowIndex(10, iFirstReactionIndex);
 
-   Mat3x3 R1(pNode1->GetRCurr());
-   Mat3x3 R2(pNode2->GetRCurr());
-   Mat3x3 RRf(pNodeRef->GetRCurr());
+   Mat3x3 R1r(pNode1->GetRCurr()*R1rtilde);
+   Mat3x3 R2r(pNode2->GetRCurr()*R2rtilde);
+   Mat3x3 RRf(pNodeRef->GetRCurr());     
    
-   Vec3 theta1(RotManip::VecRot(R1.MulTM(RRf)*R1tilde));
-   Vec3 theta2(RotManip::VecRot(R2.MulTM(RRf)*R2tilde));
+   Vec3 theta1(RotManip::VecRot(R1r.MulTM(RRf)*R1c));
+   Vec3 theta2(RotManip::VecRot(R2r.MulTM(RRf)*R2c));
 
-   DEBUGCOUT("SimpleGearJoint::AssRes(), R1r: " << R1.MulTM(RRf)*R1tilde << std::endl);
-   DEBUGCOUT("SimpleGearJoint::AssRes(), R2r: " << R2.MulTM(RRf)*R2tilde << std::endl);      
-   DEBUGCOUT("SimpleGearJoint::AssRes(), Norm[theta1]: " << theta1.Norm() << std::endl);
-   DEBUGCOUT("SimpleGearJoint::AssRes(), Norm[theta2]: " << theta2.Norm() << std::endl);      
-   DEBUGCOUT("SimpleGearJoint::AssRes(), theta1: " << theta1 << std::endl);
-   DEBUGCOUT("SimpleGearJoint::AssRes(), theta2: " << theta2 << std::endl);   
-   DEBUGCOUT("SimpleGearJoint::AssRes(), ThetaOut1: " << ThetaOut1 << std::endl);
-   DEBUGCOUT("SimpleGearJoint::AssRes(), thetaOut2: " << ThetaOut2 << std::endl);   
+   DEBUGCOUT("GearJoint::AssRes(), theta1: " << theta1 << std::endl);
+   DEBUGCOUT("GearJoint::AssRes(), theta2: " << theta2 << std::endl);   
+   DEBUGCOUT("GearJoint::AssRes(), ThetaOut1: " << ThetaOut1 << std::endl);
+   DEBUGCOUT("GearJoint::AssRes(), thetaOut2: " << ThetaOut2 << std::endl);   
    
-   //unwrapping function:
-   theta1 = Unwrap(ThetaOut1, theta1);
-   theta2 = Unwrap(ThetaOut2, theta2);
-
    Mat3x3 Gamma1IT(RotManip::DRot_IT(theta1));
    Mat3x3 Gamma2IT(RotManip::DRot_IT(theta2));
    
-   doublereal MTmp = XCurr(iFirstReactionIndex);
-   Vec3 C1(R1*Gamma1IT*kvector*Gear_r1*MTmp);   
-   Vec3 C2(R2*Gamma2IT*kvector*Gear_r2*MTmp);
+   MTmp = XCurr(iFirstReactionIndex);
+   Vec3 C1(R1r*Gamma1IT*kvector*Gear_r1*MTmp);   
+   Vec3 C2(R2r*Gamma2IT*kvector*Gear_r2*MTmp);
 
    WorkVec.Sub(1, C1);
    WorkVec.Sub(3 + 1, C2);
    WorkVec.Add(6 + 1, C1+C2);
+
+  // Unwrap angle:
+   theta1 = Unwrap(ThetaOut1, theta1);
+   theta2 = Unwrap(ThetaOut2, theta2);
     
-   DEBUGCOUT("SimpleGearJoint::AssRes(), theta1 unwrapped: " << theta1 << std::endl);
-   DEBUGCOUT("SimpleGearJoint::AssRes(), theta2 unwrapped: " << theta2 << std::endl);
-   
+   DEBUGCOUT("GearJoint::AssRes(), theta1 unwrapped: " << theta1 << std::endl);
+   DEBUGCOUT("GearJoint::AssRes(), theta2 unwrapped: " << theta2 << std::endl);
+   DEBUGCOUT("GearJoint::AssRes(), C1, C2: " << C1 << ", " << C2 << std::endl);
+
    doublereal th1z(theta1*kvector);
    doublereal th2z(theta2*kvector);
 
@@ -350,20 +385,20 @@ SimpleGearJoint::AssRes(SubVectorHandler& WorkVec,
 }
 
 unsigned int
-SimpleGearJoint::iGetNumPrivData(void) const
+GearJoint::iGetNumPrivData(void) const
 {
 	return 0;
 }
 
 int
-SimpleGearJoint::iGetNumConnectedNodes(void) const
+GearJoint::iGetNumConnectedNodes(void) const
 {
 	//Useful, but not essential:
 	return 3;
 }
 
 void
-SimpleGearJoint::GetConnectedNodes(std::vector<const Node *>& connectedNodes) const
+GearJoint::GetConnectedNodes(std::vector<const Node *>& connectedNodes) const
 {
 	//Useful, but not essential:
 	connectedNodes.resize(3);
@@ -373,7 +408,7 @@ SimpleGearJoint::GetConnectedNodes(std::vector<const Node *>& connectedNodes) co
 }
 
 void
-SimpleGearJoint::SetValue(DataManager *pDM,
+GearJoint::SetValue(DataManager *pDM,
 	VectorHandler& X, VectorHandler& XP,
 	SimulationEntity::Hints *ph)
 {
@@ -381,19 +416,19 @@ SimpleGearJoint::SetValue(DataManager *pDM,
 }
 
 std::ostream&
-SimpleGearJoint::Restart(std::ostream& out) const
+GearJoint::Restart(std::ostream& out) const
 {
-	return out << "# SimpleGearJoint: not implemented" << std::endl;
+	return out << "# GearJoint: not implemented" << std::endl;
 }
 
 unsigned int
-SimpleGearJoint::iGetInitialNumDof(void) const
+GearJoint::iGetInitialNumDof(void) const
 {
 	return 0;
 }
 
 void 
-SimpleGearJoint::InitialWorkSpaceDim(
+GearJoint::InitialWorkSpaceDim(
 	integer* piNumRows,
 	integer* piNumCols) const
 {
@@ -402,7 +437,7 @@ SimpleGearJoint::InitialWorkSpaceDim(
 }
 
 VariableSubMatrixHandler&
-SimpleGearJoint::InitialAssJac(
+GearJoint::InitialAssJac(
 	VariableSubMatrixHandler& WorkMat, 
 	const VectorHandler& XCurr)
 {
@@ -415,7 +450,7 @@ SimpleGearJoint::InitialAssJac(
 }
 
 SubVectorHandler& 
-SimpleGearJoint::InitialAssRes(
+GearJoint::InitialAssRes(
 	SubVectorHandler& WorkVec,
 	const VectorHandler& XCurr)
 {
@@ -429,48 +464,43 @@ SimpleGearJoint::InitialAssRes(
 
 
 unsigned int
-SimpleGearJoint::iGetNumDof(void) const
+GearJoint::iGetNumDof(void) const
 {
    return 1;
 }
 
  
 DofOrder::Order
-SimpleGearJoint::GetDofType(unsigned int i) const
+GearJoint::GetDofType(unsigned int i) const
 {
    return DofOrder::ALGEBRAIC;
 }
 
 
 void
-SimpleGearJoint::AfterConvergence(const VectorHandler& X,
+GearJoint::AfterConvergence(const VectorHandler& X,
 	const VectorHandler& XP)
 {
-   Mat3x3 R1(pNode1->GetRCurr());
-   Mat3x3 R2(pNode2->GetRCurr());
+   Mat3x3 R1r(pNode1->GetRCurr()*R1rtilde);
+   Mat3x3 R2r(pNode2->GetRCurr()*R2rtilde);
    Mat3x3 RRf(pNodeRef->GetRCurr());
    
-   Vec3 theta1(RotManip::VecRot(R1.MulTM(RRf)*R1tilde));
-   Vec3 theta2(RotManip::VecRot(R2.MulTM(RRf)*R2tilde));
+   Vec3 theta1(RotManip::VecRot(R1r.MulTM(RRf)*R1c));
+   Vec3 theta2(RotManip::VecRot(R2r.MulTM(RRf)*R2c));
 
    // Calculation of couples M1 and M2 (global reference):
    Mat3x3 Gamma1IT(RotManip::DRot_IT(theta1));
    Mat3x3 Gamma2IT(RotManip::DRot_IT(theta2));
    
+   // Lagrange multiplier:
    doublereal MTmp = X(iGetFirstIndex()+1);
 
-   M1 = R1*Gamma1IT*kvector*Gear_r1*MTmp;   
-   M2 = R2*Gamma2IT*kvector*Gear_r2*MTmp;
-
-   DEBUGCOUT("SGJoint::AfterConv(), theta1, ThetaOut1: " << theta1 << ", " << ThetaOut1 << std::endl);
-   DEBUGCOUT("SGJoint::AfterConv(), theta2, ThetaOut2: " << theta2 << ", " << ThetaOut2 << std::endl);
+   M1 = R1r*Gamma1IT*kvector*Gear_r1*MTmp;   
+   M2 = R2r*Gamma2IT*kvector*Gear_r2*MTmp;
 
    // Actual rotation angles calculation (local reference):  
    ThetaOut1 = Unwrap(ThetaOut1, theta1);
    ThetaOut2 = Unwrap(ThetaOut2, theta2);
-
-   DEBUGCOUT("SGJoint::AfterConv(), theta1, ThetaOut1: " << theta1 << ", " << ThetaOut1 << std::endl);
-   DEBUGCOUT("SGJoint::AfterConv(), theta2, ThetaOut2: " << theta2 << ", " << ThetaOut2 << std::endl);
 
 }
 
@@ -478,9 +508,9 @@ SimpleGearJoint::AfterConvergence(const VectorHandler& X,
 extern "C" int
 module_init(const char *module_name, void *pdm, void *php)
 {
-	UserDefinedElemRead *rf = new UDERead<SimpleGearJoint>;
+	UserDefinedElemRead *rf = new UDERead<GearJoint>;
 
-	if (!SetUDE("SimpleGearJoint", rf)) {
+	if (!SetUDE("GearJoint", rf)) {
 		delete rf;
 
 		silent_cerr("module-fabricate: "
