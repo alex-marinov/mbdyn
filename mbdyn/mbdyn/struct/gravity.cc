616fa8735d2c7c5778e59ded4fe35999f7f83696
masarati <>
2014-09-09 14:43:31 +0000

    cleanup

--- mbdyn/mbdyn/struct/gravity.cc
+++ mbdyn/mbdyn/struct/gravity.cc
@@ -1,6 +1,6 @@
 /* $Header$ */
-/* 
- * MBDyn (C) is a multibody analysis code. 
+/*
+ * MBDyn (C) is a multibody analysis code.
  * http://www.mbdyn.org
  *
  * Copyright (C) 1996-2014
@@ -17,7 +17,7 @@
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation (version 2 of the License).
- * 
+ *
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -37,57 +37,56 @@
 
 /* Gravity - begin */
 
-Gravity::Gravity(const TplDriveCaller<Vec3>* pDC, 
-		 flag fOut)
+Gravity::Gravity(const TplDriveCaller<Vec3>* pDC, flag fOut)
 : Elem(1, fOut), TplDriveOwner<Vec3>(pDC)
 {
-   Acc = Get();
+	Acc = Get();
 }
-
 
 Gravity::~Gravity(void)
 {
-   NO_OP;
+	NO_OP;
 }
-
 
 /* Scrive il contributo dell'elemento al file di restart */
-std::ostream& Gravity::Restart(std::ostream& out) const
+std::ostream&
+Gravity::Restart(std::ostream& out) const
 {
-   return out << "  gravity: /*reference, global,*/ ",
-     pGetDriveCaller()->Restart(out) << ";" << std::endl;
+	return out << "  gravity: /*reference, global,*/ ",
+		pGetDriveCaller()->Restart(out) << ";" << std::endl;
 }
 
-
-   /* assemblaggio jacobiano */
-VariableSubMatrixHandler& Gravity::AssJac(VariableSubMatrixHandler& WorkMat,
-					  doublereal /* dCoef */ , 
-					  const VectorHandler& /* XCurr */ ,
-					  const VectorHandler& /* XPrimeCurr */ )
+/* assemblaggio jacobiano */
+VariableSubMatrixHandler&
+Gravity::AssJac(VariableSubMatrixHandler& WorkMat,
+	doublereal /* dCoef */ ,
+	const VectorHandler& /* XCurr */ ,
+	const VectorHandler& /* XPrimeCurr */ )
 {
-   DEBUGCOUT("Entering Gravity::AssJac()" << std::endl);
-   WorkMat.SetNullMatrix();
-   return WorkMat;
+	DEBUGCOUT("Entering Gravity::AssJac()" << std::endl);
+	WorkMat.SetNullMatrix();
+	return WorkMat;
 }
-   
 
 /* assemblaggio residuo */
-SubVectorHandler& Gravity::AssRes(SubVectorHandler& WorkVec,
-				  doublereal /* dCoef */ ,
-				  const VectorHandler& /* XCurr */ , 
-				  const VectorHandler& /* XPrimeCurr */ )
+SubVectorHandler&
+Gravity::AssRes(SubVectorHandler& WorkVec,
+	doublereal /* dCoef */ ,
+	const VectorHandler& /* XCurr */ ,
+	const VectorHandler& /* XPrimeCurr */ )
 {
-   DEBUGCOUT("Entering Gravity::AssRes()" << std::endl);
-   WorkVec.Resize(0);
-   
-   /* Approfitto del fatto che Gravity viene aggiornato prima 
-    * degli altri elementi (vedi l'enum Elem::Type e la sequenza di
-    * assemblaggio) per fargli calcolare Acc una volta per tutte.
-    * Quindi, quando viene chiamata GetAcceleration(void), 
-    * questa restituisce un reference all'accelerazione con il
-    * minimo overhead */
-   Acc = Get();
-   return WorkVec;
+	DEBUGCOUT("Entering Gravity::AssRes()" << std::endl);
+	WorkVec.Resize(0);
+
+	/* Approfitto del fatto che Gravity viene aggiornato prima
+	 * degli altri elementi (vedi l'enum Elem::Type e la sequenza di
+	 * assemblaggio) per fargli calcolare Acc una volta per tutte.
+	 * Quindi, quando viene chiamata GetAcceleration(void),
+	 * questa restituisce un reference all'accelerazione con il
+	 * minimo overhead
+	 */
+	Acc = Get();
+	return WorkVec;
 }
 
 void
@@ -107,33 +106,34 @@ Gravity::Output(OutputHandler& OH) const
 /* GravityOwner - begin */
 
 GravityOwner::GravityOwner(void)
-: pGravity(NULL)
+: pGravity(0)
 {
-   NO_OP;
+	NO_OP;
 }
 
 
 GravityOwner::~GravityOwner(void)
 {
-   NO_OP;
+	NO_OP;
 }
 
 
-void GravityOwner::PutGravity(const Gravity* pG)
+void
+GravityOwner::PutGravity(const Gravity* pG)
 {
-   ASSERT(pGravity == NULL);
-   (Gravity*&)pGravity = (Gravity*)pG;
+	ASSERT(pGravity == 0);
+	pGravity = const_cast<Gravity *>(pG);
 }
 
-
-bool GravityOwner::bGetGravity(const Vec3& X, Vec3& Acc) const
+bool
+GravityOwner::bGetGravity(const Vec3& X, Vec3& Acc) const
 {
-   if(pGravity == NULL) {
-      return false;
-   }
-   
-   Acc = pGravity->GetAcceleration(X);
-   return true;
+	if (pGravity == 0) {
+		return false;
+	}
+
+	Acc = pGravity->GetAcceleration(X);
+	return true;
 }
 
 /* GravityOwner - end */
@@ -141,17 +141,15 @@ bool GravityOwner::bGetGravity(const Vec3& X, Vec3& Acc) const
 
 /* ElemGravityOwner - begin */
 
-ElemGravityOwner::ElemGravityOwner(unsigned int uL,
-				   flag fOut)
+ElemGravityOwner::ElemGravityOwner(unsigned int uL, flag fOut)
 : Elem(uL, fOut), GravityOwner()
 {
-   NO_OP;
+	NO_OP;
 }
-
 
 ElemGravityOwner::~ElemGravityOwner(void)
 {
-   NO_OP;
+	NO_OP;
 }
 
 /* ElemGravityOwner - end */
