53d8e5f5cc2035bfa6165b1fb931877d76e13a9b
masarati <>
2015-06-17 14:29:04 +0000

    (almost) ready for release

--- mbdyn/bootstrap.sh
+++ mbdyn/bootstrap.sh
@@ -7,6 +7,7 @@ libtoolize --automake --force --copy
 autoheader
 automake --foreign --add-missing --copy --force-missing
 autoconf
+#autoreconf -f -i -s
 
 if test -d contrib ; then
 	for i in `find contrib -name 'bootstrap.sh'` ; do
