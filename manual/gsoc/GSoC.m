3142d84669cabc81eb98dbb2b371b9c67dc14e50
Andrea Zanoni <andrea.zanoni@polimi.it>
2019-03-01 18:45:03 +0100

    Added main file for GSoC information

---
+++ manual/gsoc/GSoC.m
@@ -0,0 +1,7 @@
+# Information about Google Summer of Code
+
+MBDyn will partecipate to the 2019 edition of the [Google Summer of Code](http://summerofcode.withgoogle.com/)!
+If you are interested in partecipating with us, here you can find information about:
+  * [Project Ideas](https://gitlab.polimi.it/Pub/mbdyn/blob/master/manual/Project-Ideas.md)
+  * [Our Entry Test](https://gitlab.polimi.it/Pub/mbdyn/blob/master/manual/Entry-Test.md)
+  * [FAQs](https://gitlab.polimi.it/Pub/mbdyn/blob/master/manual/Frequently-Asked-Questions.md)
