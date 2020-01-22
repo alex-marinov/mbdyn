alex-marinov/mbdyn

commit 5c0edb6df75d6b3848cd465b43bf8964d9d5c5df

get rid of most warnings in .cc and .c files

libraries/libmbc/mbc.c
@@ -136,7 +136,7 @@ mbc_get_cmd(mbc_t *mbc)
 #ifdef _WIN32
 		winsock_err_string(err, msg);
 #else
-		*msg = "\0";
+		*msg = '0';
 #endif /* _WIN32 */
 
 		fprintf(stderr, "recv(cmd=%lu) failed, rc: %d, sizeof(mbc->cmd): %d, errno: %d, err msg: %s\n",
@@ -312,6 +312,10 @@ mbc_inet_init(mbc_t *mbc, const char *host, short unsigned port)
 	}
 
 	int serr = mbdyn_make_inet_socket(&mbc->sock, &addr, host, port, 0, NULL);
+	if (!serr) {
+		fprintf(stderr, "unable to make inet socket\n");
+		return -1;
+	}
 
 	return mbc_init(mbc, (struct sockaddr *)&addr, sizeof(addr));
 }

libraries/libmbc/sock.c
@@ -160,12 +160,14 @@ mbdyn_make_inet_socket_type(SOCKET* sock, struct sockaddr_in *name, const char *
 
    	/* disable Nagle's algorithm */
 	int flag = 1;
-	int result = setsockopt(*sock,            /* socket affected */
+	int result = setsockopt(*sock,           /* socket affected */
 				IPPROTO_TCP,     /* set option at TCP level */
 				TCP_NODELAY,     /* name of option */
 				(char *) &flag,  /* the cast is historical cruft */
 				sizeof(int));    /* length of option value */
-
+	if (!result) {
+		fprintf(stderr, "Unable to diable Nagle's algorithm, sockets may be slow\n");
+	}
 	if (dobind) {
 		rc = bind(*sock, (struct sockaddr *) name, sizeof(struct sockaddr_in));
 		if (rc == SOCKET_ERROR) {

libraries/libmbutil/bufmod.h
@@ -91,7 +91,7 @@ static T
 mbswap(const T in)
 {
 	const char *pin = (const char *)&in;
-	T out;
+	T out = T();
 	char *pout = (char *)&out;
 
 	for (unsigned int i = 0; i < sizeof(T)/2; i++) {

libraries/libmbwrap/metiswrap.cc
@@ -56,6 +56,8 @@ mbdyn_METIS_PartGraph(int iTotVertices,
 	/* required by METIS_PartGraphVKway API, but otherwise ignored */
 	/* 0: C-style numbering [0..n-1]; 1: F77-style numbering (1..n) */
 	int	numflag = 0;
+		// silence "set but not used warning if METIS_PartGraphVKway is not used
+		(void)numflag; 
 	/* if options[0] == 0, the rest is ignored */
 	int	options[5] = { 0 };
 	/* total communication volume */

libraries/libmbwrap/wraptest.cc
@@ -440,6 +440,8 @@ main(int argc, char *argv[])
 	int size = 3;
 	long long tf;
 	unsigned preord = COLAMD_PREORD;
+		//silence "set but not used" warning if SUPERLU is not used
+		(void) preord;
 	SolutionManager::ScaleWhen ms = SolutionManager::SCALEW_NEVER;
 	
 	while (1) {

mbdyn/base/ScalarFunctionsImpl.cc
@@ -1339,15 +1339,15 @@ ScalarFunctionIsotropicCLR<T, Tder>::Read(const DataManager* pDM,
 
 	CLType = ConstLawType::ELASTIC;
 
-	int n = 0;
+	//int n = 0;
 	if (typeid(T) == typeid(doublereal)) {
-		n = 1;
+		//n = 1;
 
 	} else if (typeid(T) == typeid(Vec3)) {
-		n = 3;
+		//n = 3;
 
 	} else if (typeid(T) == typeid(Vec6)) {
-		n = 6;
+		//n = 6;
 
 	} else {
 		silent_cerr("ScalarFunctionIsotropicCL"
@@ -1507,7 +1507,7 @@ InitSF(void)
 		return;
 	}
 
-	bool b;
+	bool b; (void)b; //silence set but not used warning: b used only with -DDEBUG
 
 	b = SetSF("const", new ConstSFR);
 	ASSERT(b);

mbdyn/base/inusetest.cc
@@ -134,7 +134,7 @@ main(int argc, char* argv[])
 {
 	unsigned size = 1000;
 	unsigned nt = 1;
-	unsigned loops = 1;
+	//unsigned loops = 1;
 
 	if (argc == 1) {
 usage:;
@@ -147,7 +147,7 @@ usage:;
 		}
 
 		std::cout << "usage: " << s << " [lnsSt]" << std::endl
-			<< "\t-l <loops>" << std::endl
+			//<< "\t-l <loops>" << std::endl
 			<< "\t-n <size>" << std::endl
 			<< "\t-s <sleeptime>" << std::endl
 			<< "\t-S <random sleeptime>" << std::endl
@@ -157,17 +157,18 @@ usage:;
 
 	while (true) {
 		char	*next;
-		int	opt = getopt(argc, argv, "l:n:s:S:t:");
+		//int	opt = getopt(argc, argv, "l:n:s:S:t:");
+		int	opt = getopt(argc, argv, "n:s:S:t:");
 
 		if (opt == EOF) {
 			break;
 		}
 
 		switch (opt) {
-		case 'l':
-			loops = strtoul(optarg, &next, 10);
-			break;
-
+// 		case 'l':
+// 			loops = strtoul(optarg, &next, 10);
+// 			break;
+// 
 		case 'n':
 			size = strtoul(optarg, &next, 10);
 			break;

mbdyn/base/invdataman.cc
@@ -458,6 +458,7 @@ DataManager::AssConstrRes(VectorHandler& ResHdl,
 					integer iFirstIndex = pNode->iGetFirstIndex();
 
 					const Vec3& VPrev(pNode->GetVPrev());
+						(void) VPrev; // silence set but not used warning
 					const Vec3& XPrev(pNode->GetXPrev());
 					const Vec3& XCurr(pNode->GetXCurr());
 
@@ -480,6 +481,7 @@ DataManager::AssConstrRes(VectorHandler& ResHdl,
 					}
 
 					const Vec3& WPrev(pNode->GetWPrev());
+						(void) WPrev; // silence set but not used warning
 					const Mat3x3& RPrev(pNode->GetRPrev());
 					const Mat3x3& RCurr(pNode->GetRCurr());
 
@@ -543,6 +545,7 @@ DataManager::AssConstrRes(VectorHandler& ResHdl,
 					integer iFirstIndex = pNode->iGetFirstIndex();
 
 					const Vec3& XPPPrev(pNode->GetXPPPrev());
+						(void) XPPPrev; // silence set but not used warning
 					const Vec3& VPrev(pNode->GetVPrev());
 					const Vec3& VCurr(pNode->GetVCurr());
 
@@ -566,6 +569,7 @@ DataManager::AssConstrRes(VectorHandler& ResHdl,
 					}
 
 					const Vec3& WPPrev(pNode->GetWPPrev());
+						(void) WPPrev; // silence set but not used warning
 					const Vec3& WPrev(pNode->GetWPrev());
 					const Vec3& WCurr(pNode->GetWCurr());
 

commit da943286539b0149fb90771f0fddce7c929a2a31

mbc.c: fix bug introduced by Commit 5c0edb6d which did wrong check on serr and make error output more descriptive

libraries/libmbc/mbc.c
@@ -1,5 +1,5 @@
-/* 
- * MBDyn (C) is a multibody analysis code. 
+/*
+ * MBDyn (C) is a multibody analysis code.
  * http://www.mbdyn.org
  *
  * Copyright (C) 1996-2017
@@ -16,7 +16,7 @@
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation (version 2 of the License).
- * 
+ *
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -311,9 +311,14 @@ mbc_inet_init(mbc_t *mbc, const char *host, short unsigned port)
 		return -1;
 	}
 
-	int serr = mbdyn_make_inet_socket(&mbc->sock, &addr, host, port, 0, NULL);
-	if (!serr) {
-		fprintf(stderr, "unable to make inet socket\n");
+	int sock_err;
+	int serr = mbdyn_make_inet_socket(&mbc->sock, &addr, host, port, 0, &sock_err);
+	if (serr != 0) {
+		fprintf( stderr,
+		         "unable to make inet socket, mbdyn_make_inet_socket Err code was %d\nsocket error code was %d\nSocket error message string is %s\n",
+		         serr,
+		         sock_err,
+		         sock_err_string(sock_err));
 		return -1;
 	}
 
@@ -390,7 +395,7 @@ mbc_rigid_init(mbc_rigid_t *mbc, unsigned refnode,
 	mbc->r_d_label = -1;
 	mbc->r_d_f = -1;
 	mbc->r_d_m = -1;
-	
+
 	if (!refnode) {
 		return 0;
 	}
@@ -704,7 +709,7 @@ mbc_nodal_init(mbc_nodal_t *mbc, unsigned refnode, unsigned nodes,
 		return -1;
 	}
 	MBC_F_SET(mbc, (rot & MBC_REF_NODE_ROT_MASK));
-	
+
 	mbc->n_ptr = NULL;
 	mbc->n_k_labels = NULL;
 	mbc->n_k_x = NULL;