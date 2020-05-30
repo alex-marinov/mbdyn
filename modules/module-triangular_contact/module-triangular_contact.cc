a765f99e16831cf56e21ab30f26b877ab3ed7acb
Reinhard Resch <mbdyn-user@a1.net>
2020-05-30 16:09:12 +0200

    First test with friction

--- modules/module-triangular_contact/module-triangular_contact.cc
+++ modules/module-triangular_contact/module-triangular_contact.cc
@@ -53,9 +53,271 @@
 #include "module-triangular_contact.h"
 
 #ifdef USE_AUTODIFF
+
+class LugreFriction
+{
+	public:
+	LugreFriction(const DataManager* pDM);
+	LugreFriction(const LugreFriction& oFrictModel)=default;
+	~LugreFriction();
+	void ParseInput(const Elem* pParent,
+			MBDynParser& HP);
+	void GetFrictionForce(const grad::Vector<doublereal, 2>& U,
+			      doublereal p,
+			      grad::Vector<doublereal, 2>& tau);
+	template <grad::index_type N>
+	void GetFrictionForce(const grad::Vector<grad::Gradient<N>, 2>& U,
+			      const grad::Gradient<N>& p,
+			      grad::Vector<grad::Gradient<N>, 2>& tau);
+
+	void AfterConvergence();
+	private:
+	template <typename T>
+	void GetFrictionForceTpl(const grad::Vector<T, 2>& U,
+				 const T& p,
+				 grad::Vector<T, 2>& tau);
+
+	void SaveStictionState(const grad::Vector<doublereal, 2>& z,
+			       const grad::Vector<doublereal, 2>& zP);
+	template <grad::index_type N>
+	void SaveStictionState(const grad::Vector<grad::Gradient<N>, 2>& z,
+			       const grad::Vector<grad::Gradient<N>, 2>& zP);
+
+	const DataManager* const pDM;
+	grad::Matrix<doublereal, 2, 2> Mk, Mk2, invMk2_sigma0, Ms, Ms2, sigma0, sigma1;
+	doublereal beta, vs, gamma;
+	grad::Vector<doublereal, 2> zPrev, zCurr, zPPrev, zPCurr;
+	doublereal tPrev, tCurr;
+};
+
+LugreFriction::LugreFriction(const DataManager* pDM)
+	:pDM(pDM),
+	 beta(1.),
+	 vs(0.),
+	 gamma(1.)
+{
+	tCurr = tPrev = pDM->dGetTime();
+}
+
+LugreFriction::~LugreFriction()
+{
+
+}
+
+void LugreFriction::ParseInput(const Elem* pParent, MBDynParser& HP)
+{
+	using namespace grad;
+
+	if (HP.IsKeyWord("method")) {
+		if (HP.IsKeyWord("explicit" "euler")) {
+			beta = 0.;
+		} else if (HP.IsKeyWord("implicit" "euler")) {
+			beta = 1.;
+		} else if (HP.IsKeyWord("trapezoidal" "rule")) {
+			beta = 0.5;
+		} else if (HP.IsKeyWord("custom")) {
+			beta = HP.GetReal();
+		} else {
+			silent_cerr("triangular surface contact("
+				    << pParent->GetLabel()
+				    << "): keyword \"explicit euler\", "
+				    "\"implicit euler\", "
+				    "\"trapezoidal rule\" or \"custom\" "
+				    "expected at line "
+				    << HP.GetLineData() << std::endl);
+
+			throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+		}
+	}
+
+	if (!(HP.IsKeyWord("coulomb" "friction" "coefficient")
+	      || HP.IsKeyWord("coulomb" "friction" "coefficient" "x"))) {
+		silent_cerr("triangular surface contact("
+			    << pParent->GetLabel()
+			    << "): keyword \"coulomb friction coefficient\""
+			    " or \"coulomb friction coefficient x\" expected at line "
+			    << HP.GetLineData() << std::endl);
+
+		throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+	}
+
+	const doublereal mukx = HP.GetReal();
+
+	doublereal muky;
+
+	if (HP.IsKeyWord("coulomb" "friction" "coefficient" "y")) {
+		muky = HP.GetReal();
+	} else {
+		muky = mukx;
+	}
+
+	doublereal musx, musy;
+
+	if (HP.IsKeyWord("static" "friction" "coefficient")
+	    || HP.IsKeyWord("static" "friction" "coefficient" "x")) {
+		musx = HP.GetReal();
+
+		if (HP.IsKeyWord("static" "friction" "coefficient" "y")) {
+			musy = HP.GetReal();
+		} else {
+			musy = musx;
+		}
+	} else {
+		musx = mukx;
+		musy = muky;
+	}
+
+	if (HP.IsKeyWord("sliding" "velocity" "coefficient")) {
+		vs = HP.GetReal();
+	} else {
+		vs = 1.;
+	}
+
+	if (HP.IsKeyWord("sliding" "velocity" "exponent")) {
+		gamma = HP.GetReal();
+	} else {
+		gamma = 1.;
+	}
+
+	if (!(HP.IsKeyWord("micro" "slip" "stiffness") || HP.IsKeyWord("micro" "slip" "stiffness" "x"))) {
+		silent_cerr("triangular surface contact("
+			    << pParent->GetLabel()
+			    << "): keyword \"micro slip stiffness\" or \"micro slip stiffness x\" expected at line "
+			    << HP.GetLineData() << std::endl);
+
+		throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+	}
+
+	const doublereal sigma0x = HP.GetReal();
+
+	doublereal sigma0y;
+
+	if (HP.IsKeyWord("micro" "slip" "stiffness" "y")) {
+		sigma0y = HP.GetReal();
+	} else {
+		sigma0y = sigma0x;
+	}
+
+	doublereal sigma1x, sigma1y;
+
+	if (HP.IsKeyWord("micro" "slip" "damping") || HP.IsKeyWord("micro" "slip" "damping" "x")) {
+		sigma1x = HP.GetReal();
+
+		if (HP.IsKeyWord("micro" "slip" "damping" "y")) {
+			sigma1y = HP.GetReal();
+		} else {
+			sigma1y = sigma1x;
+		}
+	} else {
+		sigma1x = 0.;
+		sigma1y = 0.;
+	}
+
+	Mk(1, 1) = mukx;
+	Mk(2, 2) = muky;
+
+	Mk2 = Mk * Mk;
+
+	Ms(1, 1) = musx;
+	Ms(2, 2) = musy;
+
+	Ms2 = Ms * Ms;
+
+	sigma0(1, 1) = sigma0x;
+	sigma0(2, 2) = sigma0y;
+
+	sigma1(1, 1) = sigma1x;
+	sigma1(2, 2) = sigma1y;
+
+	invMk2_sigma0 = Inv(Mk2) * sigma0;
+}
+
+void LugreFriction::GetFrictionForce(const grad::Vector<doublereal, 2>& U,
+				     doublereal p,
+				     grad::Vector<doublereal, 2>& tau)
+{
+	GetFrictionForceTpl(U, p, tau);
+}
+
+template <grad::index_type N>
+void LugreFriction::GetFrictionForce(const grad::Vector<grad::Gradient<N>, 2>& U,
+				     const grad::Gradient<N>& p,
+				     grad::Vector<grad::Gradient<N>, 2>& tau)
+{
+	GetFrictionForceTpl(U, p, tau);
+}
+
+void LugreFriction::AfterConvergence()
+{
+	tPrev = tCurr;
+	zPrev = zCurr;
+	zPPrev = zPCurr;
+}
+
+template <typename T>
+void LugreFriction::GetFrictionForceTpl(const grad::Vector<T, 2>& U,
+					const T& p,
+					grad::Vector<T, 2>& tau)
+{
+	using namespace grad;
+
+	typedef Matrix<doublereal, 2, 2> CMat2x2;
+	typedef Vector<doublereal, 2> CVec2;
+	typedef Matrix<T, 2, 2> VMat2x2;
+	typedef Vector<T, 2> VVec2;
+
+	const VVec2 Ueff = U * doublereal(p > 0.);
+
+	const T norm_Ueff = Dot(Ueff, Ueff);
+
+	T kappa;
+
+	if (norm_Ueff == 0.) {
+		kappa = 0.;
+	} else {
+		const VVec2 Mk_U = Mk * Ueff;
+		const VVec2 Ms_U = Ms * Ueff;
+		const VVec2 Mk2_U = Mk2 * Ueff;
+		const VVec2 Ms2_U = Ms2 * Ueff;
+		const T norm_Mk2_U = sqrt(Dot(Mk2_U, Mk2_U));
+		const T a0 = norm_Mk2_U / sqrt(Dot(Mk_U, Mk_U));
+		const T a1 = sqrt(Dot(Ms2_U, Ms2_U)) / sqrt(Dot(Ms_U, Ms_U));
+		const T g = a0 + (a1 - a0) * exp(-pow(sqrt(norm_Ueff) / vs, gamma));
+
+		kappa = norm_Mk2_U / g;
+	}
+
+	tCurr = pDM->dGetTime();
+
+	const doublereal dt = tCurr - tPrev;
+	const VMat2x2 A = invMk2_sigma0 * kappa;
+	const VMat2x2 B = A * (beta * dt) + CMat2x2(1., 0., 0., 1);
+
+	const Vector<T, 2> zP = Inv(B) * VVec2(Ueff - A * CVec2(zPrev + zPPrev * ((1 - beta) * dt)));
+	const Vector<T, 2> z = zPrev + (zP * beta + zPPrev * (1 - beta)) * dt;
+
+	SaveStictionState(z, zP);
+
+	tau = (sigma0 * z + sigma1 * zP) * p;
+}
+
+void LugreFriction::SaveStictionState(const grad::Vector<doublereal, 2>& z,
+				      const grad::Vector<doublereal, 2>& zP)
+{
+	zCurr = z;
+	zPCurr = zP;
+}
+
+template <grad::index_type N>
+void LugreFriction::SaveStictionState(const grad::Vector<grad::Gradient<N>, 2>&,
+				      const grad::Vector<grad::Gradient<N>, 2>&)
+{
+	// Do Nothing
+}
+
 class TriangSurfContact: virtual public Elem, public UserDefinedElem
 {
-public:
+	public:
 	TriangSurfContact(unsigned uLabel, const DofOwner *pDO,
 			  DataManager* pDM, MBDynParser& HP);
 	virtual ~TriangSurfContact(void);
@@ -96,7 +358,7 @@ public:
 	InitialAssRes(SubVectorHandler& WorkVec, const VectorHandler& XCurr);
 	virtual void SetValue(DataManager*, VectorHandler&, VectorHandler&, SimulationEntity::Hints*);
 	virtual unsigned int iGetInitialNumDof() const;
-private:
+	private:
 	struct TargetVertex {
 		Mat3x3 R = ::Zero3x3;
 		Vec3 o = ::Zero3;
@@ -104,8 +366,15 @@ private:
 
 	struct TargetFace {
 		static constexpr integer iNumVertices = 3;
-		Vec3 oc = ::Zero3;
+
+		TargetFace(const DataManager* pDM)
+			:oc(::Zero3),
+			 oFrictionModel(pDM) {
+		}
+
+		Vec3 oc;
 		std::array<TargetVertex, iNumVertices> rgVert;
+		LugreFriction oFrictionModel;
 	};
 
 	struct ContactNode {
@@ -116,16 +385,16 @@ private:
 			:pContNode(pNode),
 			 vOffset(vOffset),
 			 dRadius(dRadius)
-		{
-			rgTargetFaces.reserve(iNumFaces);
-		}
+			{
+				rgTargetFaces.reserve(iNumFaces);
+			}
 
 		void Reset() {
 			rgTargetFaces.clear();
 		}
 
 		const StructNode* const pContNode;
-		std::vector<const TargetFace*> rgTargetFaces;
+		std::vector<TargetFace*> rgTargetFaces;
 		const Vec3 vOffset;
 		const doublereal dRadius;
 	};
@@ -173,6 +442,11 @@ private:
 	grad::LocalDofMap oDofMap;
 	const DifferentiableScalarFunction* pCL;
 	static constexpr grad::index_type iNumDofGradient = 13;
+
+	enum class FrictionModel {
+		None,
+		Lugre
+	} eFrictionModel;
 };
 
 TriangSurfContact::TriangSurfContact(unsigned uLabel, const DofOwner *pDO,
@@ -181,7 +455,8 @@ TriangSurfContact::TriangSurfContact(unsigned uLabel, const DofOwner *pDO,
 	 UserDefinedElem(uLabel, pDO),
 	 pTargetNode(nullptr),
 	 dSearchRadius(std::numeric_limits<doublereal>::max()),
-	 pCL(nullptr)
+	 pCL(nullptr),
+	 eFrictionModel(FrictionModel::None)
 {
 	if (HP.IsKeyWord("help"))
 	{
@@ -221,6 +496,18 @@ TriangSurfContact::TriangSurfContact(unsigned uLabel, const DofOwner *pDO,
 		throw ErrGeneric(MBDYN_EXCEPT_ARGS);
 	}
 
+	TargetFace oCurrFace(pDM);
+
+	if (HP.IsKeyWord("friction" "model")) {
+		if (HP.IsKeyWord("lugre")) {
+			eFrictionModel = FrictionModel::Lugre;
+			oCurrFace.oFrictionModel.ParseInput(this, HP);
+		} else if (!HP.IsKeyWord("none")) {
+			silent_cerr("triangular surface(" << uLabel << "): keyword \"lugre\" or \"none\" expected at line " << HP.GetLineData() << std::endl);
+			throw ErrGeneric(MBDYN_EXCEPT_ARGS);
+		}
+	}
+
 	ReferenceFrame oRefTarget(pTargetNode);
 
 	if (!HP.IsKeyWord("number" "of" "target" "vertices")) {
@@ -256,8 +543,6 @@ TriangSurfContact::TriangSurfContact(unsigned uLabel, const DofOwner *pDO,
 	}
 
 	rgTargetMesh.reserve(iNumFaces);
-
-	TargetFace oCurrFace;
 
 	for (integer i = 0; i < iNumFaces; ++i) {
 		oCurrFace.oc = ::Zero3;
@@ -323,6 +608,9 @@ TriangSurfContact::TriangSurfContact(unsigned uLabel, const DofOwner *pDO,
 
 		rgContactMesh.emplace_back(pContNode, o1, dRadius, iNumFaces);
 	}
+
+
+
 
 	oNodeSet.reserve(iNumContactNodes);
 }
@@ -463,6 +751,8 @@ TriangSurfContact::UnivAssRes(grad::GradientAssVec<T>& WorkVec,
 	const integer iFirstIndex2 = pTargetNode->iGetFirstMomentumIndex();
 
 	Vector<T, 3> X1, X2, F1, M1, F2, M2;
+	Vector<T, 3> XP1, XP2, omega1, omega2, dV;
+	Vector<T, 2> U, tau;
 	Matrix<T, 3, 3> R1, R2;
 
 	for (const auto& rNode: rgContactMesh) {
@@ -481,17 +771,39 @@ TriangSurfContact::UnivAssRes(grad::GradientAssVec<T>& WorkVec,
 			const Mat3x3& R2i = pTargetFace->rgVert[0].R;
 			const Vec3& o2i = pTargetFace->rgVert[0].o;
 
-			const Vector<T, 3> n = R2 * R2i.GetCol(3);
+			const Vector<T, 3> n3 = R2 * R2i.GetCol(3);
 			const Vector<T, 3> l1 = R1 * o1;
 			const Vector<T, 3> l2 = X1 + l1 - X2;
-			const T dz = Dot(n, l2 - R2 * o2i) - rNode.dRadius;
+			const T dz = Dot(n3, Vector<T, 3>(l2 - R2 * o2i));
+			const Vector<T, 3> l2c = l2 + n3 * dz;
+			const Vector<T, 3> l1c = l1 - n3 * dz;
+			const T pz = dz - rNode.dRadius;
+			const T F2in = GetContactForce(pz, &oDofMap);
+			Vector<T, 3> F2i = n3 * F2in;
 
-			const Vector<T, 3> F2i = n * GetContactForce(dz, &oDofMap);
+			if (eFrictionModel == FrictionModel::Lugre) {
+				pTargetNode->GetVCurr(XP2, dCoef, func, &oDofMap);
+				pTargetNode->GetWCurr(omega2, dCoef, func, &oDofMap);
+				rNode.pContNode->GetVCurr(XP1, dCoef, func, &oDofMap);
+				rNode.pContNode->GetWCurr(omega1, dCoef, func, &oDofMap);
+
+				dV = Transpose(R2) * Vector<T, 3>(XP1 + Cross(omega1, l1c) - XP2 - Cross(omega2, l2c));
+
+				for (index_type i = 1; i <= 2; ++i) {
+					U(i) = Dot(Direct(R2i.GetCol(i)), dV);
+				}
+
+				pTargetFace->oFrictionModel.GetFrictionForce(U, T(fabs(F2in)), tau);
+
+				for (index_type i = 1; i <= 2; ++i) {
+					F2i += R2 * (Direct(R2i.GetCol(i)) * tau(i));
+				}
+			}
 
 			F2 += F2i;
-			M2 += Cross(l2, F2i);
+			M2 += Cross(l2c, F2i);
 			F1 -= F2i;
-			M1 -= Cross(l1, F2i);
+			M1 -= Cross(l1c, F2i);
 		}
 
 		const integer iFirstIndex1 = rNode.pContNode->iGetFirstMomentumIndex();
@@ -553,22 +865,27 @@ TriangSurfContact::InitialAssRes(SubVectorHandler& WorkVec, const VectorHandler&
 void TriangSurfContact::AfterConvergence(const VectorHandler& X,
 					 const VectorHandler& XP)
 {
+	if (eFrictionModel != FrictionModel::None) {
+		for (auto& oTargetFace: rgTargetMesh) {
+			oTargetFace.oFrictionModel.AfterConvergence();
+		}
+	}
 }
 #endif
 
 bool triangular_contact_set(void)
 {
 #ifdef USE_AUTODIFF
-    UserDefinedElemRead *rf = new UDERead<TriangSurfContact>;
+	UserDefinedElemRead *rf = new UDERead<TriangSurfContact>;
 
-    if (!SetUDE("triangular" "surface" "contact", rf))
-    {
-	delete rf;
-	return false;
-    }
+	if (!SetUDE("triangular" "surface" "contact", rf))
+	{
+		delete rf;
+		return false;
+	}
 #endif
 
-    return true;
+	return true;
 }
 
 #ifndef STATIC_MODULES
@@ -578,16 +895,16 @@ extern "C"
 
     int module_init(const char *module_name, void *pdm, void *php)
     {
-	if (!triangular_contact_set())
-	{
-	    silent_cerr("contact: "
-			"module_init(" << module_name << ") "
-			"failed" << std::endl);
+	    if (!triangular_contact_set())
+	    {
+		    silent_cerr("contact: "
+				"module_init(" << module_name << ") "
+				"failed" << std::endl);
 
-	    return -1;
-	}
+		    return -1;
+	    }
 
-	return 0;
+	    return 0;
     }
 
 }
