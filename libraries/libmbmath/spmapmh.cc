efd59bbe1b012b46892001af4fef70c040dd32f4
Reinhard Resch <mbdyn-user@a1.net>
2020-12-02 19:50:30 +0100

    Fix compiler error if sparse autodiff is disabled

--- libraries/libmbmath/spmapmh.cc
+++ libraries/libmbmath/spmapmh.cc
@@ -106,10 +106,78 @@ idx_type SpMapMatrixHandler::MakeCompressedColumnFormTpl(doublereal *const Ax,
 	return x_ptr;     
 }
 
-integer
+template <typename idx_type>
+idx_type SpMapMatrixHandler::MakeCompressedRowFormTpl(doublereal *const Ax,
+						      idx_type *const Ai,
+						      idx_type *const Ap,
+						      int offset) const
+{
+	row_cont_type::iterator ri;
+	row_cont_type::const_iterator re;
+
+	std::vector<idx_type> oRowSize(NRows, 0);
+
+	for (integer iCol = 0; iCol < NCols; ++iCol) {
+	     re = col_indices[iCol].end();
+	     for (ri = col_indices[iCol].begin(); ri != re; ++ri) {
+		  ASSERT(ri->first >= 0);
+		  ASSERT(ri->first < NRows);
+		  
+		  ++oRowSize[ri->first];
+	     }
+	}
+
+	idx_type iPtr = offset;
+
+	for (integer iRow = 0; iRow < NRows; ++iRow) {
+	     Ap[iRow] = iPtr;
+
+	     ASSERT(oRowSize[iRow] >= 0);
+	     ASSERT(oRowSize[iRow] <= NRows);
+	  
+	     iPtr += oRowSize[iRow];
+	}
+     
+	Ap[NRows] = iPtr;
+
+	std::fill(oRowSize.begin(), oRowSize.end(), 0);
+
+#ifdef DEBUG
+	constexpr idx_type iInvalidIndex = -1;
+	constexpr doublereal dInvalidValue = std::numeric_limits<doublereal>::infinity();
+     
+	std::fill(Ai, Ai + Nz(), iInvalidIndex);
+	std::fill(Ax, Ax + Nz(), dInvalidValue);
+#endif	
+	
+	for (integer iCol = 0; iCol < NCols; iCol++) {
+		re = col_indices[iCol].end();
+		for (ri = col_indices[iCol].begin(); ri != re; ++ri) {
+		     ASSERT(ri->first >= 0);
+		     ASSERT(ri->first < NRows);
+		     
+		     const idx_type iSlot = Ap[ri->first] - offset + oRowSize[ri->first]++;
+		     
+		     ASSERT(iSlot >= 0);
+		     ASSERT(iSlot < Ap[ri->first + 1] - offset);
+		     
+		     ASSERT(Ax[iSlot] == dInvalidValue);
+		     ASSERT(Ai[iSlot] == iInvalidIndex);
+		     
+		     Ax[iSlot] = ri->second;
+		     Ai[iSlot] = iCol + offset;
+		}
+	}
+
+	ASSERT(iPtr - offset == Nz());
+	
+	return iPtr - offset;     
+}
+
+int32_t
 SpMapMatrixHandler::MakeCompressedColumnForm(doublereal *const Ax,
-					     integer *const Ai,
-					     integer *const Ap,
+					     int32_t *const Ai,
+					     int32_t *const Ap,
 					     int offset) const
 {
      return MakeCompressedColumnFormTpl(Ax, Ai, Ap, offset);
@@ -124,12 +192,31 @@ SpMapMatrixHandler::MakeCompressedColumnForm(doublereal *const Ax,
      return MakeCompressedColumnFormTpl(Ax, Ai, Ap, offset);
 }
 
-integer
-SpMapMatrixHandler::MakeIndexForm(doublereal *const Ax,
-		integer *const Arow, integer *const Acol,
-		integer *const Ap, int offset) const
+int32_t
+SpMapMatrixHandler::MakeCompressedRowForm(doublereal *const Ax,
+					     int32_t *const Ai,
+					     int32_t *const Ap,
+					     int offset) const
 {
-	integer x_ptr = 0;
+     return MakeCompressedRowFormTpl(Ax, Ai, Ap, offset);
+}
+
+int64_t
+SpMapMatrixHandler::MakeCompressedRowForm(doublereal *const Ax,
+					     int64_t *const Ai,
+					     int64_t *const Ap,
+					     int offset) const
+{
+     return MakeCompressedRowFormTpl(Ax, Ai, Ap, offset);
+}
+
+template <typename idx_type>
+idx_type
+SpMapMatrixHandler::MakeIndexFormTpl(doublereal *const Ax,
+				     idx_type *const Arow, idx_type *const Acol,
+				     idx_type *const Ap, int offset) const
+{
+	idx_type x_ptr = 0;
 
 	row_cont_type::iterator ri;
 	row_cont_type::const_iterator re;
@@ -150,20 +237,23 @@ SpMapMatrixHandler::MakeIndexForm(doublereal *const Ax,
 
 	Ap[NCols] = x_ptr + offset;
 
-	return Nz();
+	return x_ptr;
 }
 
-integer
-SpMapMatrixHandler::MakeIndexForm(std::vector<doublereal>& Ax,
-                std::vector<integer>& Arow, std::vector<integer>& Acol,
-		std::vector<integer>& Ap, int offset) const
+int32_t SpMapMatrixHandler::MakeIndexForm(doublereal *const Ax,
+					  int32_t *const Arow, int32_t *const Acol,
+					  int32_t *const AcolSt,
+					  int offset) const
 {
-	Ax.resize(Nz());
-	Arow.resize(Nz());
-	Acol.resize(Nz());
-	Ap.resize(iGetNumCols() + 1);
+     return MakeIndexFormTpl(Ax, Arow, Acol, AcolSt, offset);
+}
 
-	return MakeIndexForm(&Ax[0], &Arow[0], &Acol[0], &Ap[0], offset);
+int64_t SpMapMatrixHandler::MakeIndexForm(doublereal *const Ax,
+					  int64_t *const Arow, int64_t *const Acol,
+					  int64_t *const AcolSt,
+					  int offset) const
+{
+     return MakeIndexFormTpl(Ax, Arow, Acol, AcolSt, offset);
 }
 
 void
@@ -473,11 +563,4 @@ std::ostream& SpMapMatrixHandler::Print(std::ostream& os, MatPrintFormat eFormat
     } else {
         return SparseMatrixHandler::Print(os, eFormat);
     }
-}
-
-void SpMapMatrixHandler::ForceSymmetricGraph()
-{
-     for (const auto& oItem: *this) {
-	  (*this)(oItem.iCol + 1, oItem.iRow + 1);
-     }
 }
